25c16abf8f851b184c48140ff3afd2c083ab93a3
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-05-18 17:53:21 +0530

    Time: 13 ms (89.84%), Space: 16.6 MB (51.72%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -2,21 +2,18 @@ class Solution {
 public:
     int jump(vector<int>& nums) {
         int n=nums.size();
-        vector<int>dis(n,INT_MAX);
+        int i=0,maxReachable=0,lastReached=0,step=0;
         
-        if(n==1)
-            return 0;
-        
-        dis[0]=0;
-        
-        for(int i=0;i<n;i++){
-            for(int j=1;j<=nums[i];j++){
-                dis[i+j]=min(dis[i+ j],dis[i]+1);
-                if(dis[n-1]!=INT_MAX)
-                    return dis[n-1];
+        while(lastReached<n-1){
+            maxReachable=max(maxReachable,i+nums[i]);
+            if(i==lastReached){
+                lastReached=maxReachable;
+                step++;
             }
-                
+            i++;
         }
-        return dis[n-1];
+        
+        return step;
+        
     }
 };
