804374d25ee23dc58fe2cb938860c71f0d25a1b6
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-05-23 12:46:30 +0530

    Time: 20 ms (42.49%), Space: 19.7 MB (6.05%) - LeetHub

---
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int ans=0;
+        int i=0,j=height.size()-1;
+        int maxL=height[i],maxR=height[j];
+        
+        while(i<j){
+            maxL=max(maxL,height[i]);
+            maxR=max(maxR,height[j]);
+            if(maxL<=maxR){
+                ans+=(maxL-height[i++]);
+           }
+            else{
+                ans+=(maxR-height[j--]);
+            }
+        }
+        return ans;
+    }
+};
