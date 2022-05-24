cb68e04733682a894a866b6a021d9b399fc7d38f
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-05-24 11:33:01 +0530

    Time: 205 ms (14.63%), Space: 6 MB (80.44%) - LeetHub

--- 60-permutation-sequence/60-permutation-sequence.cpp
+++ 60-permutation-sequence/60-permutation-sequence.cpp
@@ -4,9 +4,27 @@ public:
         string ans="";
         for(int i=1;i<=n;i++)
             ans+='0'+i;
-        
+        if(n==1||k==1)
+            return ans;
         while(--k){
-            next_permutation(ans.begin(),ans.end());
+         int idx1,idx2;
+            for(int i=n-2;i>=0;i--){
+                if(ans[i]<ans[i+1]){
+                    idx1=i;
+                    break;
+                }
+            }
+            
+            for(int i=n-1;i>=0;i--){
+                if(ans[i]>ans[idx1]){
+                    idx2=i;
+                    break;
+                }
+            }
+            
+            swap(ans[idx1],ans[idx2]);
+            reverse(ans.begin()+idx1+1,ans.end());
+            
         }
         
         return ans;
