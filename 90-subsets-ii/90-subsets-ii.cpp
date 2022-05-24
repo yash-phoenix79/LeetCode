48a7396a61d04aae8c7a6e05d41984128a7e708c
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-05-24 21:00:04 +0530

    Time: 9 ms (29.30%), Space: 7.9 MB (49.86%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,31 +1,20 @@
 class Solution {
 public:
-    
-    void findSubsets(int idx,int n,vector<int>temp, set<vector<int>>&s,vector<int>& nums){
-        if(idx==n){
-           sort(temp.begin(),temp.end());
-            s.insert(temp);
-            return;
+    void findSets(vector<int>& nums,int idx,vector<int>ds, vector<vector<int>>&ans){
+        ans.push_back(ds);
+        for(int i=idx;i<nums.size();i++){
+            if(i!=idx&&nums[i]==nums[i-1])continue;
+            ds.push_back(nums[i]);
+            findSets(nums,i+1,ds,ans);
+            ds.pop_back();
         }
-          findSubsets(idx+1,n,temp,s,nums);
-        temp.push_back(nums[idx]);
-        findSubsets(idx+1,n,temp,s,nums);
-        
-        
-        
     }
     
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector<vector<int>> ans;
-        set<vector<int>>s;
-        vector<int>temp;
-        
-        int n=nums.size();
-        findSubsets(0,n,temp,s,nums);
-        
-        
-        for(auto x:s)
-        ans.push_back(x);
+        vector<vector<int>>ans;
+        vector<int>ds;
+        sort(nums.begin(),nums.end());
+        findSets(nums,0,ds,ans);
         return ans;
     }
 };
