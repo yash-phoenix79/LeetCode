edd2cacd947c28ad5e98c5f2049e5cf1fb9061df
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-05-24 19:24:29 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (17.38%) - LeetHub

--- 60-permutation-sequence/60-permutation-sequence.cpp
+++ 60-permutation-sequence/60-permutation-sequence.cpp
@@ -1,33 +1,26 @@
 class Solution {
 public:
     string getPermutation(int n, int k) {
-        string ans="";
-        for(int i=1;i<=n;i++)
-            ans+='0'+i;
-        if(n==1||k==1)
-            return ans;
-        while(--k){
-         int idx1,idx2;
-            for(int i=n-2;i>=0;i--){
-                if(ans[i]<ans[i+1]){
-                    idx1=i;
-                    break;
-                }
-            }
-            
-            for(int i=n-1;i>=0;i--){
-                if(ans[i]>ans[idx1]){
-                    idx2=i;
-                    break;
-                }
-            }
-            
-            swap(ans[idx1],ans[idx2]);
-            reverse(ans.begin()+idx1+1,ans.end());
-            
+       vector<int>t;
+        int fact=1;
+        for(int i=1;i<n;i++){
+            fact*=i;
+           t.push_back(i);
         }
+        t.push_back(n);
         
+        string ans="";
+        k--;
+        int temp=n-1;
+        while(true){
+            ans+=to_string(t[k/fact]);
+            t.erase(t.begin()+(k/fact));
+            if(t.empty())
+                break;
+            k%=fact;
+            fact/=temp;
+            temp--;
+        }
         return ans;
-        
     }
 };
