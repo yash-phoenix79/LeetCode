bcaf57224a5f99135f4fca4c3510131391c4e7a1
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-05-26 17:29:30 +0530

    Time: 528 ms (41.23%), Space: 77.6 MB (44.26%) - LeetHub

--- 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
@@ -1,44 +1,18 @@
 class Solution {
-    static bool cmp(vector<int>& a, vector<int>& b){
-        if(a[0]==b[0]) return a[1] > b[1];
-        return a[0] < b[0];
-    }
 public:
-    int maxEnvelopes(vector<vector<int>>& env) {
-        int n = env.size();
+    int maxEnvelopes(vector<vector<int>>& e) {
+        sort(e.begin(),e.end(),[](vector<int>&v1,vector<int>&v2){
+            if(v1[0]==v2[0])return v1[1]>v2[1];
+            return v1[0]<v2[0];
+        });
         
-        // sorting by height & if we encounter same height
-        // sort by descending order of width
-        sort(env.begin(), env.end(), cmp);
+        vector<int>res;
         
-        // e.g. -> env => (3,8) (4,5) (2,1) (2,6) (7,8) (5,3) (5,7)
-        // sorted version => (2,6) (2,1) (3,8) (4,5) (5,7) (5,3) (7,8)
-        
-        // Now, we only need to care about width
-        // So, as of now we only need to look upon v[i][1]
-        // [ 6, 1, 8, 5, 7, 3, 8 ]
-        
-        // Now, we can only put envolop a in envolop b another if width of a is
-        // less than width of b, or we can say we need an envolop whose width
-        // is just greater than envolop a ( height we have already handled )
-        // So, we can think of lower_bound and Longest Increasing Subsequence
-        
-        // because we have sorted all envolopes of a particular height
-        // by descending order of width, one envolope will not interfare with 
-        // another envolop of same height, if we apply lower_bound
-        // i.e. first elem greater than equal to itself in lis
-        
-        vector<int> lis;
-        
-        for(int i = 0;i<env.size();i++){
-            int ele = env[i][1];
-            
-            int idx = lower_bound(lis.begin(), lis.end(), ele) - lis.begin();
-            
-            if(idx == lis.size()) lis.push_back(ele);
-            else lis[idx] = ele;
+        for(int i=0;i<e.size();i++){
+            int idx=lower_bound(res.begin(),res.end(),e[i][1])-res.begin();
+            if(idx==res.size())res.push_back(e[i][1]);
+            else res[idx]=e[i][1];
         }
-        
-        return lis.size();
+        return res.size();
     }
 };
