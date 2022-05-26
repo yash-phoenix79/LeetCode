49f5e55079a1817d18890eb4e11c8972557ac332
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-05-26 16:54:57 +0530

    Time: 45 ms (35.29%), Space: 6.3 MB (82.28%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.cpp
+++ 37-sudoku-solver/37-sudoku-solver.cpp
@@ -1,40 +1,36 @@
 class Solution {
 public:
     
-    bool check(vector<vector<char>>& b,int i,int j,int num){
+    bool check(vector<vector<char>>& b,int i,int j,char c){
+       int  t1=i-i%3,t2=j-j%3;
         
-        char c='0'+num;
-       int row=i-i%3,col=j-j%3;
-        
-      for(int x=0;x<9;x++)if(b[x][j]==c)return false;
-      for(int y=0;y<9;y++)if(b[i][y]==c)return false;
-      for(int x=0;x<3;x++){
-          for(int y=0;y<3;y++){
-              if(b[x+row][y+col]==c)return false;
-          }
-      }
-        
-        
+        for(int x=0;x<9;x++)if(b[x][j]==c)return false;
+        for(int y=0;y<9;y++)if(b[i][y]==c)return false;
+        for(int x=0;x<3;x++){
+            for(int y=0;y<3;y++){
+                if(b[t1+x][t2+y]==c)return false;
+            }
+        }
         return true;
     }
     
-    bool findSol(int i,int j,vector<vector<char>>& b){
-        if(i==9)return true;
-        if(j==9){
-           return findSol(i+1,0,b);
+    
+    bool findSol(int x,int y,vector<vector<char>>& b){
+        if(x==9)
+            return true;
+        if(y==9){
+            return findSol(x+1,0,b);
         }
+        if(b[x][y]!='.')return findSol(x,y+1,b);
         
-        if(b[i][j]!='.')return findSol(i,j+1,b);
-        
-        for(int n=1;n<=9;n++){
-           
-            if(check(b,i,j,n)){
-                 b[i][j]='0'+n;
-                if(findSol(i,j+1,b)) return true;
-                 b[i][j]='.';
+        for(char c='1';c<='9';c++){
+            if(check(b,x,y,c)){
+                b[x][y]=c;
+                if(findSol(x,y+1,b))return true; 
+                b[x][y]='.';
             }
-           
         }
+        
         return false;
     }
     
