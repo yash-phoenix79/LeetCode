6f086e40f4d16e0d52139240d5311e37cc79a729
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-05-29 17:15:23 +0530

    Time: 62 ms (67.22%), Space: 15.5 MB (89.46%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int maxProduct(vector<string>& words) {
-        int n=size(words),ans=0;
+        int n=words.size();
         vector<int>mask(n,0);
-        for(int i=0;i<n;i++)
+        for(int i=0;i<n;i++){
             for(auto &ch:words[i])
-                mask[i]|=(1<<(ch-'a'));
+                mask[i]|=1<<(ch-'a');
+        }
+        int ans=0;
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
-                if(__builtin_popcount(mask[i]&mask[j])==0)
-                    ans=max(ans,int(size(words[i])*size(words[j])));
+                if((mask[i]&mask[j])==0)
+                   ans=max(ans,int (size(words[i])*size(words[j]))); 
             }
         }
         return ans;
