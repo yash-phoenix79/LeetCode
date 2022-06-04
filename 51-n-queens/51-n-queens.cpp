d1bbbd10b202415f13cb2b6a445ce129999d438b
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-06-04 12:10:31 +0530

    Time: 81 ms (11.85%), Space: 50.7 MB (5.44%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,67 +1,50 @@
 class Solution {
 public:
-    vector<vector<string>> ans;
+     vector<vector<string>> ans;
     
-    bool check(int x,int y,vector<vector<char>>&Queens){
-        int n=Queens.size();
+    bool check(int y,int x,vector<string>board)
+    {
+    int n=board.size();
         for(int i=0;i<n;i++){
-            if(Queens[i][y]=='Q')return false;
+            if(board[i][y]=='Q')
+                return false;
         }
-        
-        int i=x,j=y;
-        
-        while(i<n&&j<n){
-            if(Queens[i][j]=='Q')return false;
-            i++,j++;
-        }
-        i=x,j=y;
-        while(i<n&&j>=0){
-            if(Queens[i][j]=='Q')return false;
-            i++,j--;
-        }
-        i=x,j=y;
+          int i=x,j=y;
         while(i>=0&&j>=0){
-            if(Queens[i][j]=='Q')return false;
-            i--,j--;
+            if(board[i--][j--]=='Q')
+                return false;
         }
         i=x,j=y;
-         while(i>=0&&j<n){
-            if(Queens[i][j]=='Q')return false;
-            i--,j++;
+        while(i>=0&&j<n){
+            if(board[i--][j++]=='Q')
+                return false;
         }
-        
         return true;
-        
-    }
+    }   
     
-    void findQueens(int i,int n,vector<vector<char>>&Queens){
-        if(i==n){
-            vector<string>res;
-            for(int idx=0;idx<n;idx++){
-                string t="";
-                for(int j=0;j<n;j++)
-                    t+=Queens[idx][j];
-                res.push_back(t);
-            }
-            ans.push_back(res);
-            return ;
-        }
-        
-        for(int j=0;j<n;j++){
-            if(check(i,j,Queens))
-            {
-                Queens[i][j]='Q';
-                findQueens(i+1,n,Queens);
-                Queens[i][j]='.';
+    void solve(int i,int n,vector<string>board){
+     if(i==n)
+     {
+         ans.push_back(board);
+         return;
+     }
+        for(int idx=0;idx<n;idx++){
+            if(check(idx,i,board)){
+                board[i][idx]='Q';
+                solve(i+1,n,board);
+                board[i][idx]='.';
             }
         }
-        
-        return ;
     }
     
     vector<vector<string>> solveNQueens(int n) {
-         vector<vector<char>> Queens(n,vector<char>(n,'.'));
-        findQueens(0,n,Queens);
+        
+        vector<string>board(n,string(n,'.'));
+        // // string t(n,'.');
+        // for(int i=0;i<n;i++){
+        //     board[i]=t;
+        // }
+        solve(0,n,board);
         return ans;
     }
 };
