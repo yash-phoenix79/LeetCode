6aaef5ccd960e8b69e8785852bab65a3e4913184
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-06-07 22:32:14 +0530

    Time: 3 ms (53.80%), Space: 6.5 MB (52.29%) - LeetHub

---
+++ 58-length-of-last-word/58-length-of-last-word.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int ans=0;
+        int i=s.size()-1;
+        while(i>=0&&s[i]==' ')
+            i--;
+        
+        while(i>=0&&s[i]!=' ')
+        {
+            ans++;
+            i--;
+        }
+        return ans;
+    }
+};
