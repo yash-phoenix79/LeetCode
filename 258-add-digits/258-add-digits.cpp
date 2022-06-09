178a9655837dc96b7d035b88cd145134d396b5bc
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-06-09 10:43:18 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (25.58%) - LeetHub

--- 258-add-digits/258-add-digits.cpp
+++ 258-add-digits/258-add-digits.cpp
@@ -1,18 +1,10 @@
 class Solution {
 public:
     int addDigits(int num) {
-        int sum=0;
-        
-        while(num/10){
-            sum=0;
-            while(num){
-            sum+=(num%10);
-            num/=10;
-            
-        }
-            num=sum;
-        }
-        return num;
-        
+        if(num==0)
+            return 0;
+        if(num%9==0)
+            return 9;
+            return num%9;
     }
 };
