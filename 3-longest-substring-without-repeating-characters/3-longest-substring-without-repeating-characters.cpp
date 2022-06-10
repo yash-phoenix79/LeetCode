845cb0c49c4917ed41417f1bf355f00728c6061d
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-06-10 11:40:24 +0530

    Time: 4 ms (96.12%), Space: 8.3 MB (73.87%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        if(s.empty())
-            return 0;
-        int n=s.length();
-        int start=-1;
-        int ans=0;
-       vector<int>al(256,-1);
-        
-        for(int i=0;i<n;i++){
-            if(al[s[i]-'\0']>start)
-                start=al[s[i]-'\0'];
-            al[s[i]-'\0']=i;
-            ans=max(ans,i-start);
+       vector<int>ch(256,-1);
+        int start=-1,maxLen=0;
+        for(int i=0;i<s.length();i++){
+            
+            if(start<ch[s[i]-'\0'])
+                start=ch[s[i]-'\0'];
+            maxLen=max(maxLen,i-start);
+            ch[s[i]-'\0']=i;
         }
-        return ans;
+        return maxLen;
     }
 };
