9e879172f7e78d16c5d9c84d4edb576ebb933dca
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-06-16 00:02:41 +0530

    Time: 6 ms (37.51%), Space: 6.7 MB (67.25%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -1,18 +1,9 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-       vector<int>alph(26,0);
-        
-        for(int i=0;i<t.length();i++)
-            alph[t[i]-'a']++;
-        for(int i=0;i<s.length();i++)
-             alph[s[i]-'a']--;
-        char ans;
-        for(int i=0;i<26;i++){
-            if(alph[i])
-               { ans= 'a'+i;
-                break;}
-        }
-        return ans;
+        char c=0;
+        for(auto x:s)c^=x;
+        for(auto x:t)c^=x;
+        return c;
     }
 };
