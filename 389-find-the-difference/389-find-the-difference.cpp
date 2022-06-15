25a98b04bc081559dcb3e1ade290087c6ea7c97c
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-06-16 00:05:56 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (26.06%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        char c=0;
-        for(auto x:s)c^=x;
-        for(auto x:t)c^=x;
-        return c;
+      int ans=0;
+        for(auto x:t)
+            ans+=x;
+        for(auto x:s)
+            ans-=x;
+        return (char)ans;
     }
 };
