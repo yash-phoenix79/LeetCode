80de7280dde781cda909493efb4e761ffc6609af
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-06-22 13:21:21 +0530

    Time: 13 ms (38.16%), Space: 10.5 MB (32.19%) - LeetHub

--- 414-third-maximum-number/414-third-maximum-number.cpp
+++ 414-third-maximum-number/414-third-maximum-number.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
-        unordered_set<int>s;
-        for(int i=0;i<nums.size();i++)
-            s.insert(nums[i]);
         
-        vector<int>temp;
- for(auto it=s.begin();it!=s.end();it++)
-     temp.push_back(*it);   
-     
-        sort(temp.begin(),temp.end(),greater<int>());
-        
-        if(temp.size()<3)
-            return (temp[0]);
-        return temp[2];
+        set<int>s;
+       for(int i:nums){
+           
+            s.insert(i);
+           if(s.size()>3)
+               s.erase(s.begin());
+       }
+          
+        return s.size()==3?*(s.begin()):*(s.rbegin());
         
     }
 };
