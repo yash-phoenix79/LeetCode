da98dcccfba272a56c2f27afd2ef389efbe34cda
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-06-23 21:10:17 +0530

    Time: 12 ms (18.48%), Space: 8.1 MB (13.52%) - LeetHub

---
+++ 214-shortest-palindrome/214-shortest-palindrome.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    string shortestPalindrome(string s) {
+        int l=s.length();
+        if(l==0)
+            return "";
+        if(l==1)
+            return s;
+        
+        string t=s,y=s;
+        reverse(s.begin(),s.end());
+        if(s==y)
+            return s;
+        t=t+"#"+s;
+        
+        int n=t.length();
+        vector<int>pref(n);
+        pref[0]=0;
+        int i=0,j=1;
+        
+        while(j<n){
+            while(i!=0&&t[i]!=t[j])
+                i=pref[i-1];
+            if(t[j]==t[i]){
+                pref[j]=i+1;
+                i++;
+                j++;
+            }
+            else{
+                pref[j]=0;
+                j++;
+            }
+        }
+        
+        int x=pref[n-1];
+        if(x==l)
+            return y;
+        string res=t.substr(x,l-x);
+        reverse(res.begin(),res.end());
+        return res+y;
+        
+    }
+};
