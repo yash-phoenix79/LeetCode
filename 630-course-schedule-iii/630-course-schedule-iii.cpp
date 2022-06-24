c2b44176250968924f80776e3d5eb11bf1f07a30
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-06-24 16:34:54 +0530

    Time: 388 ms (42.53%), Space: 63.4 MB (30.03%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.cpp
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     int scheduleCourse(vector<vector<int>>& courses) {
-        if(courses.size() <= 0) return 0;
-        sort(courses.begin(), courses.end(), [](const vector<int>& a, vector<int>& b) {
-            return a[1] < b[1];
+        
+        sort(courses.begin(),courses.end(),[](auto &a,auto &b){
+           return a[1]<b[1]; 
         });
-        priority_queue<int> q;
-        int sum = 0;
-        for(auto i : courses) {
-            sum += i[0];
-            q.push(i[0]);
-            if(sum > i[1]) {
-                sum -= q.top();
+        
+        priority_queue<int>q;
+        int tot=0;
+        
+        for(auto c:courses){
+            int dur=c[0],dead=c[1];
+            tot+=dur;
+            q.push(dur);
+            if(tot>dead){
+                tot-=q.top();
                 q.pop();
             }
         }
