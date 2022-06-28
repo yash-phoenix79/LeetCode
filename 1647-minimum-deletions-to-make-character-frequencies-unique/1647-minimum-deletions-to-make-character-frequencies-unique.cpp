ea590185637a13c2946df86c583a63a1a80351d6
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-06-28 16:20:18 +0530

    Time: 54 ms (91.97%), Space: 17.2 MB (95.28%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,34 +1,20 @@
 class Solution {
 public:
     int minDeletions(string s) {
-        
+     
         int n=s.length();
         vector<int>al(26,0);
-        
         for(int i=0;i<n;i++)
             al[s[i]-'a']++;
         
         sort(al.begin(),al.end(),greater<int>());
+        int prev=INT_MAX,keep=0;
         
-        unordered_map<int,int>m;
-        int i=25;
-        while(i>=0&&al[i]==0)
-            i--;
-        
-        int res=0;
-        while(i>=0){
-            if(m[al[i]]==0)
-                m[al[i]]++;
-            else{
-                while(al[i]!=0&&m[al[i]]!=0){
-                    res++;
-                    al[i]--;
-                }
-                if(al[i]!=0)
-                    m[al[i]]++;
-            }
-            i--;
+        for(auto x:al){
+            if(!x||!prev)break;
+            prev=min(x,prev-1);
+            keep+=prev;
         }
-        return res;
+        return n-keep;
     }
 };
