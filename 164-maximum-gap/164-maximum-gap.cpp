59e038b18ab34ad18b7acbeef812f2029e8e8b67
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-07-06 13:22:42 +0530

    Time: 263 ms (58.44%), Space: 78.5 MB (34.84%) - LeetHub

--- 164-maximum-gap/164-maximum-gap.cpp
+++ 164-maximum-gap/164-maximum-gap.cpp
@@ -1,22 +1,48 @@
 class Solution {
 public:
+    //as we were told to find the max. difference of two successive elements when they get sorted but with actually applying typical sort algorithm
+    // we imply the use of sorting algorithm with no comparator 
+    
     int maximumGap(vector<int>& nums) {
-        
         int n=nums.size();
         if(n<2)
             return 0;
+        // finding max and min element to find the value of average gap/interval
+        int maxi=*max_element(begin(nums),end(nums)); 
+        int mini=*min_element(begin(nums),end(nums));
         
-        int cur=-1,res=0;
+       
+        int inter=(int)ceil((float)(maxi-mini)/(n-1)); //average interval is found by taking the ceil value function
+        cout<<inter<<endl;
         
-        set<int>s(begin(nums),end(nums));
+        int bu=n-1;// number of intervals
         
-        for(auto x:s){
-            if(cur!=-1&&x!=cur)
-                res=max(res,x-cur);
-            cur=x;
+        vector<int>mx(bu,-1);
+        vector<int>mi(bu,INT_MAX);
+        
+        for(int i=0;i<n;i++){
+            if(nums[i]==mini||nums[i]==maxi)
+            {
+                continue;
+            }
+            int val=(nums[i]-mini)/inter;
+           mx[val]=max(mx[val],nums[i]);
+           mi[val]=min(mi[val],nums[i]);
+            
         }
         
-        return res;
         
+        int prev=mini;
+        int res=0;
+        
+        for(int i=0;i<bu;i++){
+            if(mx[i]==-1)continue;
+            res=max(res,mi[i]-prev);
+            prev=mx[i];
+        }
+        
+        res=max(maxi-prev,res);
+        
+        return res;
     }
 };
