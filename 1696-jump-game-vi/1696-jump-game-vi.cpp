1ee3206b1169df63264d925a9043f2d38111afb7
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-07-09 20:00:40 +0530

    Time: 394 ms (32.98%), Space: 77.3 MB (96.95%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.cpp
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     int maxResult(vector<int>& nums, int k) {
-        
         int n=nums.size();
         long cnt=0;
-        
         deque<int>q;
-        
         for(int i=n-1;i>=0;i--){
             cnt=nums[i]+(q.empty()?0:nums[q.front()]);
             while(!q.empty()&&cnt>=nums[q.back()])
                 q.pop_back();
             q.push_back(i);
-            if(i+k<=q.front())
+            if(i+k==q.front())
                 q.pop_front();
             nums[i]=cnt;
         }
-        
-        return cnt;
-        
+        return nums[0];
     }
 };
