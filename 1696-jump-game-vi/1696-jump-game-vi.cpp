4354f5716234a8185e35dd1a3e8f61f398e8e01e
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-07-09 20:06:36 +0530

    Time: 268 ms (64.54%), Space: 77.3 MB (99.05%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.cpp
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -4,15 +4,15 @@ public:
         int n=nums.size();
         long cnt=0;
         deque<int>q;
-        for(int i=n-1;i>=0;i--){
+        for(int i=0;i<n;i++){
             cnt=nums[i]+(q.empty()?0:nums[q.front()]);
             while(!q.empty()&&cnt>=nums[q.back()])
                 q.pop_back();
             q.push_back(i);
-            if(i+k==q.front())
+            if(q.front()==i-k)
                 q.pop_front();
             nums[i]=cnt;
         }
-        return nums[0];
+        return nums[n-1];
     }
 };
