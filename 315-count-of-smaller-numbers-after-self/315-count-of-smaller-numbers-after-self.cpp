276c39df1ee3bedfed589ad5d067c5b15e67009e
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-07-23 13:28:07 +0530

    Time: 1095 ms (32.66%), Space: 209.4 MB (40.01%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
@@ -3,33 +3,24 @@ public:
     
     void merge(vector<int>&cnt,vector<pair<int,int>>&arr,int l,int m,int r){
         vector<pair<int,int>>temp(r-l+1);
+        int i=l,j=m+1;
         int k=0;
-        int i=l;
-        int j=m+1;
         
         while(i<=m&&j<=r){
             if(arr[i].first>arr[j].first){
-                cnt[arr[i].second]+=r-j+1;
+                cnt[arr[i].second]+=(r-j+1);
                 temp[k++]=arr[i++];
             }
-                
-            else{
+            else
                 temp[k++]=arr[j++];
-            }
         }
-        
         while(i<=m)
-           temp[k++]=arr[i++];
-        
+            temp[k++]=arr[i++];
         while(j<=r)
-           temp[k++]=arr[j++];
+            temp[k++]=arr[j++];
         
         for(int i=l;i<=r;i++)
             arr[i]=temp[i-l];
-        
-        
-            
-        
     }
     
     void mergeSort(vector<int>&cnt,vector<pair<int,int>>&arr,int l,int r){
@@ -44,16 +35,11 @@ public:
     vector<int> countSmaller(vector<int>& nums) {
         int n=nums.size();
         vector<int>cnt(n,0);
-        
         vector<pair<int,int>>arr(n);
-        
         for(int i=0;i<n;i++){
             arr[i]={nums[i],i};
         }
-        
         mergeSort(cnt,arr,0,n-1);
-        
         return cnt;
-        
     }
 };
