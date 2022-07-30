a06d618c5b8b3937652b51e1976487b899baf49c
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-07-30 15:40:32 +0530

    Time: 719 ms (5.01%), Space: 19.9 MB (66.01%) - LeetHub

---
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int d[5]={0,1,0,-1,0};
+    int maxDistance(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        
+        int res=-1;
+        
+        queue<pair<int,int>>q;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j])
+                q.push({i,j});
+            }
+        }
+        int t=1;
+        while(!q.empty()){
+            int sz=q.size();
+          
+            
+            for(int i=0;i<sz;i++){
+                auto cur=q.front();
+                q.pop();
+                cout<<cur.first<<" "<<cur.second<<endl;
+                int x=cur.first;
+                int y=cur.second;
+                
+                for(int i=0;i<4;i++){
+                    int dx=x+d[i];
+                    int dy=y+d[i+1];
+                    if(dx<0||dy<0||dx>=n||dy>=m||grid[dx][dy]!=0)
+                        continue;
+                    else{
+                        grid[dx][dy]=t;
+                       res=max(res,t);
+                    q.push({dx,dy}); 
+                    }
+                    
+                }
+            }
+            cout<<endl;
+            t++;
+        }
+        return res;
+    }
+};
