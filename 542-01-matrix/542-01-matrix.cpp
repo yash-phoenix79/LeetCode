22a0afca4f35418e060063a0ba755f3d036c4515
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-08-01 15:25:06 +0530

    Time: 77 ms (95.15%), Space: 27.6 MB (75.78%) - LeetHub

--- 542-01-matrix/542-01-matrix.cpp
+++ 542-01-matrix/542-01-matrix.cpp
@@ -1,42 +1,35 @@
 class Solution {
 public:
-    int d[5]={1,0,-1,0,1};
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-
-        
         int n=mat.size();
         int m=mat[0].size();
         
-      vector<vector<int>>res(n,vector<int>(m,INT_MAX));
-        queue<pair<int,int>>q;
+        if(n==0)
+            return mat;
+        
+        vector<vector<int>>dist(n,vector<int>(m,INT_MAX - 100000));
+        
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(!mat[i][j]){
-                    res[i][j]=0;
-                    q.push({i,j});
+                if(mat[i][j]==0)
+                    dist[i][j]=0;
+                else {
+                    if(i>0)
+                        dist[i][j]=min(dist[i][j],1+dist[i-1][j]);
+                    if(j>0)
+                        dist[i][j]=min(dist[i][j],1+dist[i][j-1]);
                 }
             }
         }
         
-        while(!q.empty()){
-         
-                auto cur=q.front();
-                q.pop();
-                int x=cur.first;
-                int y=cur.second;
-                for(int i=0;i<4;i++){
-                    int dx=x+d[i];
-                    int dy=y+d[i+1];
-                    if(dx<0||dy<0||dx>=n||dy>=m)
-                     continue;
-                   if(res[dx][dy]>1+res[x][y]){
-                        res[dx][dy]=1+res[x][y];
-                       q.push({dx,dy});
-                    }
-                     
-                }
-            
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(i<n-1)
+                    dist[i][j]=min(dist[i][j],1+dist[i+1][j]);
+                if(j<m-1)
+                    dist[i][j]=min(dist[i][j],1+dist[i][j+1]);
+            }
         }
-        return res;
+        return dist;
     }
 };
