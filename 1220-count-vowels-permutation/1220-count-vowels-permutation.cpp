3d47f47446de63d8406486522f382cc1992ff02d
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-08-07 18:55:51 +0530

    Time: 6 ms (87.38%), Space: 5.7 MB (100.00%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -1,44 +1,24 @@
 class Solution {
 public:
-    #define N 1000000007
- 
-   int find(int idx,int n,vector<vector<int>>&dp,vector<vector<int>>&vowels){
-       
-       if(dp[n][idx]!=-1)
-           return dp[n][idx];
-       
-       int sum=0;
-       
-   
-       for(int j=0;j<vowels[idx].size();j++){
-           sum=(sum+find(vowels[idx][j],n-1,dp,vowels))%N;
-       } 
-        // cout<<sum<<endl;
     
-       
-       return dp[n][idx]=sum;
-   }
+    #define N 1000000007
     
     int countVowelPermutation(int n) {
-   
-        vector<vector<int>>vowels(5);
-        vowels[0]={1};
-        vowels[1]={0,2};
-        vowels[2]={0,1,3,4};
-        vowels[3]={2,4};
-        vowels[4]={0};
+        long a=1,e=1,i=1,o=1,u=1;
         
-        vector<vector<int>>dp(n+1,vector<int>(5,-1));
+        long a2,e2,i2,o2,u2;
         
-        for(int i=0;i<5;i++){
-            dp[0][i]=0;
-            dp[1][i]=1;
+        for(int j=2;j<=n;j++){
+            a2=(e+i+u)%N;
+            e2=(a+i)%N;
+            i2=(e+o)%N;
+            o2=i;
+            u2=(i+o)%N;
+            
+            a=a2,e=e2,i=i2,o=o2,u=u2;
         }
         
-        int res=0;
-        for(int i=0;i<5;i++)
-       res=(res+find(i,n,dp,vowels))%N;
-   
-        return res;
+        return (a+e+i+o+u)%N;
+        
     }
 };
