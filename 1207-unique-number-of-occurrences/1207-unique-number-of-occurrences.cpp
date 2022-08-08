94f64034c8e7487c9f1f6c6ccaaaa69c0f0065db
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-08-08 12:44:25 +0530

    Time: 8 ms (28.66%), Space: 8.2 MB (49.74%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        int n=size(arr);
+        
+        unordered_map<int,int>m;
+        for(int x:arr)
+            m[x]++;
+        
+        unordered_set<int>s;
+        for(auto x:m){
+            if(s.find(x.second)!=s.end())
+                return false;
+            s.insert(x.second);
+        }
+        
+        return true;
+    }
+};
