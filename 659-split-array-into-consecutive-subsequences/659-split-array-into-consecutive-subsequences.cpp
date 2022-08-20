7ac5f7c1d85f7d9acb70a5acbb1d649abad520f2
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-08-20 17:25:03 +0530

    Time: 169 ms (49.11%), Space: 58.3 MB (66.47%) - LeetHub

--- 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
@@ -1,50 +1,25 @@
 class Solution {
 public:
-    //app 1: using greedy approach in O(N) 
-    
-/* for each num in nums first try placing it in one of the existing subseq.
-if no subseq. needs that number.
-then, try creating a new subseq. of atleast length 3 starting with that num.
-
-if neither of the two condtn is true, we return false
-since, that num can't be a part of any subseq.
-
-*/
     bool isPossible(vector<int>& nums) {
-        //freq. map denotes no. of elements left to be placed in subseq.
-        unordered_map<int,int> freq;
-        for(int x: nums) freq[x]++;
-        
-        //hypothetical map which denotes the "next" number req. by subsequences.
-        unordered_map<int,int> need;
-        
-        for(int n: nums){
-            //all occurences of cur num is already taken
-            if(freq[n] == 0) continue;
-            
-            if(need[n] > 0){ // "n" can be a part of some existing subseq.
-                need[n]--;
-                freq[n]--;
-                
-                need[n+1]++; //next req. num is now "N+1"
+        unordered_map<int,int>freq,end;
+        for(auto x:nums)
+            freq[x]++;
+        for(auto x:nums){
+            if(freq[x]==0)continue;
+            freq[x]--;
+            if(end[x-1]>0)
+            {
+                end[x-1]--;
+                end[x]++;
             }
-            
-            //try creating a new sub. of length atleast three
-            else if(freq[n]>0 && freq[n+1]>0 && freq[n+2]>0){
-                freq[n]--;
-                freq[n+1]--;
-                freq[n+2]--;
-                
-                //next num needed in subseq.
-                need[n+3]++;
+            else if(freq[x+1]>0&&freq[x+2]>0){
+                freq[x+1]--;
+                freq[x+2]--;
+                end[x+2]++;
             }
-            
-            //above both condtn is false
-            else{
+            else
                 return false;
-            }
         }
-        
         return true;
     }
 };
