6a3be14db706d46dff01acff3bb9591ebb1e1d90
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-08-27 15:26:01 +0530

    Time: 146 ms (47.80%), Space: 12 MB (80.20%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,53 +1,37 @@
 class Solution {
 public:
-    
-   vector<vector<pair<int,int>>>adj; 
-    vector<pair<int,int>>res;
-    
-    void dijkstra(int v,int n,int thres){
-        vector<int>dis(n,INT_MAX);
-        dis[v]=0;
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-        pq.push({v,0});
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        vector<vector<int>>adj(n,vector<int>(n,INT_MAX/2));
         
-        while(!pq.empty()){
-            auto cur=pq.top();
-            pq.pop();
-            int ver=cur.first;
-            int w=cur.second;
-            for(auto x:adj[ver]){
-                if(dis[x.first]>x.second+w){
-                    dis[x.first]=w+x.second;
-                    pq.push({x.first,dis[x.first]});
+        for(int i=0;i<n;i++)
+            adj[i][i]=0;
+        for(int i=0;i<edges.size();i++){
+            adj[edges[i][0]][edges[i][1]]=edges[i][2];
+            adj[edges[i][1]][edges[i][0]]=edges[i][2];
+        }
+            
+        
+        for(int k=0;k<n;k++){
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    adj[i][j]=min(adj[i][j],adj[i][k]+adj[k][j]);
                 }
             }
         }
         
-        int cnt=0;
+        int res=-1,minCnt=INT_MAX;
+        
         for(int i=0;i<n;i++){
-            if(i!=v&&dis[i]<=thres)
-                cnt++;
+           int cnt=0;
+            for(int j=0;j<n;j++){
+                if(adj[i][j]<=distanceThreshold)
+                    cnt++;
+            }
+            if(cnt<=minCnt){
+                minCnt=cnt;
+                res=i;
+            }
         }
-        
-        res.push_back({v,cnt});
-        
-    }
-    
-    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        adj=vector<vector<pair<int,int>>>(n);
-        for(int i=0;i<edges.size();i++){
-            adj[edges[i][0]].push_back({edges[i][1],edges[i][2]});
-            adj[edges[i][1]].push_back({edges[i][0],edges[i][2]});
-        }
-        
-        for(int i=0;i<n;i++)
-            dijkstra(i,n,distanceThreshold);
-        
-        sort(res.begin(),res.end(),[](auto &a,auto &b){
-           if(a.second==b.second)
-               return a.first>b.first;
-            return a.second<b.second;
-        });
-        return res[0].first;
+        return res;
     }
 };
