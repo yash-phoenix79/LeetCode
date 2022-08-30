45e65f58979ea2efe3b4ce846d015862d72e5dba
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-08-30 20:23:19 +0530

    Time: 41 ms (30.87%), Space: 14.9 MB (71.43%) - LeetHub

---
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> shortestAlternatingPaths(int n, vector<vector<int>>& re, vector<vector<int>>& bl) {
+        
+        vector<vector<pair<int,int>>>adj(n);
+        for(auto &x:re)adj[x[0]].push_back({x[1],0});
+        for(auto &x:bl)adj[x[0]].push_back({x[1],1});
+        
+        vector<vector<int>>dis(n,vector<int>(2,-1));
+        queue<pair<int,int>>q;
+        q.push({0,0});
+        q.push({0,1});
+        dis[0]={0,0};
+        
+        while(!q.empty()){
+            auto [i, c1] = q.front();
+            q.pop();
+            
+           for(auto [j, c2] : adj[i]){
+              if(dis[j][c2]!=-1||c1==c2)
+                 continue;
+               dis[j][c2]=1+dis[i][c1];
+               q.push({j,c2});
+            }
+        }
+        
+        vector<int>res;
+        for(auto &v:dis){
+            sort(begin(v),end(v));
+            res.push_back(v[0]==-1?v[1]:v[0]);
+        }
+        
+        return res;
+        
+    }
+};
