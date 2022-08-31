798ac95ce3fee315e077da50a470898e6e0f74b0
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-08-31 16:29:13 +0530

    Time: 90 ms (29.51%), Space: 30.9 MB (95.92%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,24 +1,8 @@
 class Solution {
 public:
-    vector<int>vis;
-    
-    bool find(vector<int>&arr,int start,int n){
-        if(vis[start]==1)
-            return false;
-        vis[start]=1;
-        if(arr[start]==0)
-            return true;
-        bool res=false;
-        if(start+arr[start]<n)
-            res=res||find(arr,start+arr[start],n);
-        if(start-arr[start]>=0)
-            res=res||find(arr,start-arr[start],n);
-        return res;
-    }
-    
     bool canReach(vector<int>& arr, int start) {
-        int n=arr.size();
-        vis=vector<int>(n,0);
-        return find(arr,start,n);
+        if(start<0||start>=arr.size()||arr[start]<0)return false;
+        arr[start]=-1*arr[start];
+        return arr[start]==0||canReach(arr,start+arr[start])||canReach(arr,start-arr[start]);
     }
 };
