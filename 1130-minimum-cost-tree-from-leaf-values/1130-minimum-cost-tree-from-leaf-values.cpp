af2bf43c2fa93849f5f95bc59a3ff42d8c45073d
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-09-05 21:13:17 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (55.71%) - LeetHub

--- 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
+++ 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    
-    int find(vector<int>&arr,int l,int r,vector<vector<int>>&dp){
-        if(l==r)
-            return 0;
-        if(dp[l][r]!=-1)
-            return dp[l][r];
-        
-        int res=INT_MAX;
-        for(int i=l;i<r;i++)
-            res=min(res,*max_element(arr.begin()+l,arr.begin()+i+1)*(*max_element(arr.begin()+i+1,arr.begin()+r+1))+find(arr,l,i,dp)+find(arr,i+1,r,dp));
-        return dp[l][r]=res;
-    }
-    
     int mctFromLeafValues(vector<int>& arr) {
         
-        int n=arr.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        return find(arr,0,n-1,dp);
-        
+       
+        int res=0;
+        while(arr.size()>1){
+            int t=INT_MAX;
+            int mini=-1;
+            for(int i=1;i<arr.size();i++)
+            {
+
+                if(arr[i]*arr[i-1]<t){
+                    mini=arr[i]<arr[i-1]?i:i-1;
+                    t=arr[i]*arr[i-1];
+                }
+            }
+            res+=t;
+            arr.erase(arr.begin()+mini);
+        }
+        return res;
     }
 };
