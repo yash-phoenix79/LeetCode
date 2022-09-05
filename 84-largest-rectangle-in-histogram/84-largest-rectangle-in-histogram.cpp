97f68c13354fa44b8a8d86fc200a7a4ee263c916
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-09-06 01:07:26 +0530

    Time: 393 ms (16.76%), Space: 81.5 MB (45.47%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -8,33 +8,30 @@ public:
         vector<int>left(n,0),right(n,n-1);
         
         for(int i=0;i<n;i++){
+             while(!s.empty()&&hei[i]<=hei[s.top()])
+                    s.pop();
             if(s.empty())
                 left[i]=0;
-            else{
-                while(!s.empty()&&hei[i]<=hei[s.top()])
-                    s.pop();
-                left[i]=s.empty()?0:s.top()+1;
-            }
+            else
+                left[i]=s.top()+1;
+            
             s.push(i);
         }
         
-        // for(int i=0;i<n;i++)
-        //     cout<<left[i]<<" ";
-        // cout<<endl;
+      
         stack<int>st;
         for(int i=n-1;i>=0;i--){
+            while(!st.empty()&&hei[i]<=hei[st.top()])
+                    st.pop();
+               
             if(st.empty())
                 right[i]=n-1;
-            else{
-                while(!st.empty()&&hei[i]<=hei[st.top()])
-                    st.pop();
-                right[i]=st.empty()?n-1:st.top()-1;
-            }
+            else
+                right[i]=st.top()-1; 
+            
             st.push(i);
         }
         
-        // for(int i=0;i<n;i++)
-        //     cout<<right[i]<<" ";
         
         int res=0;
         for(int i=0;i<n;i++)
