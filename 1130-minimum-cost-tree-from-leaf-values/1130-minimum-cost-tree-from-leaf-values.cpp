ecf3404231d6a64f674c249428173e00801f6cd3
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-09-05 21:06:57 +0530

    Time: 43 ms (27.37%), Space: 8.9 MB (45.27%) - LeetHub

--- 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
+++ 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
@@ -1,32 +1,23 @@
 class Solution {
 public:
     
-    int find(vector<int>& arr,map<pair<int,int>,int>&m,int l,int r,vector<vector<int>>&dp){
+    int find(vector<int>&arr,int l,int r,vector<vector<int>>&dp){
         if(l==r)
             return 0;
-        
         if(dp[l][r]!=-1)
             return dp[l][r];
         
         int res=INT_MAX;
         for(int i=l;i<r;i++)
-        res=min(res,m[{l,i}]*m[{i+1,r}]+find(arr,m,l,i,dp)+find(arr,m,i+1,r,dp));
+            res=min(res,*max_element(arr.begin()+l,arr.begin()+i+1)*(*max_element(arr.begin()+i+1,arr.begin()+r+1))+find(arr,l,i,dp)+find(arr,i+1,r,dp));
         return dp[l][r]=res;
     }
     
     int mctFromLeafValues(vector<int>& arr) {
+        
         int n=arr.size();
-        map<pair<int,int>,int>m;
-        
         vector<vector<int>>dp(n,vector<int>(n,-1));
-        
-        for(int i=0;i<n;i++){
-            m[{i,i}]=arr[i];
-            for(int j=i+1;j<n;j++)
-                m[{i,j}]=max(arr[j],m[{i,j-1}]);
-        }
-        
-        return find(arr,m,0,n-1,dp);
+        return find(arr,0,n-1,dp);
         
     }
 };
