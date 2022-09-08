3cd9b6a22c0cab7f71d02d5a7b0113a6d35cb622
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-09-08 19:32:45 +0530

    Time: 43 ms (5.16%), Space: 7.9 MB (45.31%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
-      
-    int findPath(int i,int j,vector<vector<int>>& obs,vector<vector<int>>&dp){
+    
+    int find(int i,int j,vector<vector<int>>& obs,vector<vector<int>>&dp){
+        int n=obs.size();
+        int m=obs[0].size();
         
-          int n=obs.size(),m=obs[0].size();
-        
-         if(i<0||i>=n||j<0||j>=m)
+         if(i>=n||j>=m)
             return 0;
         
         if(obs[i][j])
             return dp[i][j]=0;
-      
-        if(i==n-1&&j==m-1)return 1;
-        
-        if(dp[i][j])return dp[i][j];
-        
-        
-    return  dp[i][j]=findPath(i,j+1,obs,dp)+findPath(i+1,j,obs,dp);
        
+        
+                if(i==n-1&&j==m-1)
+            return 1;
+        
+        if(dp[i][j])
+            return dp[i][j];
+
+        return dp[i][j]=find(i+1,j,obs,dp)+find(i,j+1,obs,dp);
     }
     
     int uniquePathsWithObstacles(vector<vector<int>>& obs) {
-        int n=obs.size(),m=obs[0].size();
-        if(obs[n-1][m-1])return 0;
+        int n=obs.size();
+        int m=obs[0].size();
+        
+        if(obs[n-1][m-1]||obs[0][0])return 0;
+        
         vector<vector<int>>dp(n,vector<int>(m));
-        
-        return findPath(0,0,obs,dp);
-        
-       
+        return find(0,0,obs,dp);
         
     }
 };
