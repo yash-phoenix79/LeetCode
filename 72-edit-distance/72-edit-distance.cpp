3043fed8101cc3183a722ff16b1925c554929297
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-09-08 21:08:15 +0530

    Time: 48 ms (24.97%), Space: 9.1 MB (40.50%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
+    
+    // this problem is similar to the approach of Longest Common Subsequence 
+    
     int minDistance(string word1, string word2) {
-        int m=word1.size(),n=word2.size();
-        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+     
+        int n=word1.size();//length of first string
+        int m=word2.size();//length of second string 
         
-        if(m==0&&n==0)
-            return 0;
-        
-        dp[0][0]=0;
-        for(int i=1;i<=m;i++)
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));//declaring a 2D vector 
+        for(int i=1;i<=n;i++)// if a string is not null but the other string is null,then we have no chance but to remove all the elements of the string
             dp[i][0]=i;
-        for(int i=1;i<=n;i++)
-            dp[0][i]=i;
+        for(int j=1;j<=m;j++)// if a string is not null but the other string is null,then we have no chance but to remove all the elements of the string
+            dp[0][j]=j;
         
-        for(int i=1;i<=m;i++){
-            for(int j=1;j<=n;j++){
-                if(word1[i-1]==word2[j-1])
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(word1[i-1]==word2[j-1])//if a letter in word1 is equal to a letter in word2
                     dp[i][j]=dp[i-1][j-1];
                 else{
-                    dp[i][j]=1+min({dp[i-1][j-1],dp[i-1][j],dp[i][j-1]});
+                    dp[i][j]=1+min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]});
                 }
             }
         }
-      return dp[m][n];
+        return dp[n][m];
     }
 };
