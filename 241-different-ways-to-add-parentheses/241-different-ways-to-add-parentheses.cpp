1980015807c18aeb5e55be38301bb9d3987f6d7f
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-09-09 12:12:49 +0530

    Time: 17 ms (5.88%), Space: 13.5 MB (5.57%) - LeetHub

--- 241-different-ways-to-add-parentheses/241-different-ways-to-add-parentheses.cpp
+++ 241-different-ways-to-add-parentheses/241-different-ways-to-add-parentheses.cpp
@@ -1,39 +1,36 @@
 class Solution {
 public:
     
-    vector<int> recur(string s,int l,int r){
-        if(l>r)
-            return {1};
-        if(l==r)
-            return {s[l]-'0'};
-        vector<int>res;
-        for(int i=l;i<=r;i++){
-            if(s[i]=='+'||s[i]=='-'||s[i]=='*'){
-                
-                vector<int>left=recur(s,l,i-1);
-                vector<int>right=recur(s,i+1,r);
-                
-                for(auto x:left){
-                    for(auto y:right){
-                        if(s[i]=='+')
-                            res.push_back(x+y);
-                        else if(s[i]=='-')
-                            res.push_back(x-y);
-                        else
-                            res.push_back(x*y);
-                    }
-                }
-                
-            }
-        }
-        if(res.empty())
-            return {stoi(s.substr(l,r-l+1))};
-        return res;
-        
-    }
+  vector<int> recur(string s,int l,int r){
+      if(l>r)
+          return {1};
+      if(l==r)
+          return {s[l]-'0'};
+      vector<int>res;
+      for(int i=l;i<=r;i++){
+          if(s[i]=='+'||s[i]=='-'||s[i]=='*'){
+              vector<int>a=recur(s,l,i-1);
+              vector<int>b=recur(s,i+1,r);
+              for(auto x:a){
+                  for(auto y:b){
+                      if(s[i]=='+')
+                          res.push_back(x+y);
+                      else if(s[i]=='-')
+                          res.push_back(x-y);
+                      else
+                          res.push_back(x*y);
+                  }
+              }
+              
+          }
+      }
+      if(res.empty())
+                  return {stoi(s.substr(l,r-l+1))};
+      return res;
+  }
     
     vector<int> diffWaysToCompute(string expression) {
-        int n=expression.length();
+     int n=expression.length();
         return recur(expression,0,n-1);
     }
 };
