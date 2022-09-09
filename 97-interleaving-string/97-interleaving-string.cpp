5feea268012722648d4e2508b4f718a62aadbd50
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-09-09 12:43:08 +0530

    Time: 3 ms (85.86%), Space: 6.4 MB (78.79%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
     bool isInterleave(string s1, string s2, string s3) {
-    
-    if(s3.length() != s1.length() + s2.length())
-        return false;
-    
-    bool table[s1.length()+1][s2.length()+1];
-    
-    for(int i=0; i<s1.length()+1; i++)
-        for(int j=0; j< s2.length()+1; j++){
-            if(i==0 && j==0)
-                table[i][j] = true;
-            else if(i == 0)
-                table[i][j] = ( table[i][j-1] && s2[j-1] == s3[i+j-1]);
-            else if(j == 0)
-                table[i][j] = ( table[i-1][j] && s1[i-1] == s3[i+j-1]);
-            else
-                table[i][j] = (table[i-1][j] && s1[i-1] == s3[i+j-1] ) || (table[i][j-1] && s2[j-1] == s3[i+j-1] );
-        }
+        int n=s1.length();
+        int m=s2.length();
         
-    return table[s1.length()][s2.length()];
-}
+        if(n+m!=s3.length())
+            return false;
+        
+        vector<vector<bool>>dp(n+1,vector<bool>(m+1,false));
+
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=m;j++){
+                if(i==0&&j==0)
+                    dp[i][j]=true;
+                else if(i==0)
+                    dp[i][j]=dp[i][j-1]&&s2[j-1]==s3[j-1];
+                else if(j==0)
+                    dp[i][j]=dp[i-1][j]&&s1[i-1]==s3[i-1];
+                else{
+                    dp[i][j]=(dp[i-1][j]&&s1[i-1]==s3[i+j-1])||(dp[i][j-1]&&s2[j-1]==s3[i+j-1]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
 };
