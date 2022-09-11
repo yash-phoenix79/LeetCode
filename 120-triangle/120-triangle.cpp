5727a1722f770ae7459e6a7ba16d2d7b03375cf3
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-09-11 13:18:08 +0530

    Time: 16 ms (16.61%), Space: 8.6 MB (93.53%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -6,8 +6,8 @@ public:
         for(int i=n-2;i>=0;i--){
             
             for(int j=0;j<=i;j++)
-                tri[i][j]+=min(mini[j],mini[j+1]);
-            mini=tri[i];
+                mini[j]=tri[i][j]+min(mini[j],mini[j+1]);
+            
         }
         
         return mini[0];
