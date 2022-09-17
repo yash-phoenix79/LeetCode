ce8b9e3c51bcf4f6d4af44815d3cb6c7521e58d5
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-09-17 10:42:47 +0530

    Time: 854 ms (29.28%), Space: 120 MB (65.45%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
-    vector<vector<int>>dp;
+    
+     vector<vector<int>>dp;
     
     int find(int i,int j,vector<int>& nums, vector<int>& mul){
-        
-         int n=nums.size();   
+         int n=nums.size();
         int m=mul.size();
         
-        if(j>=mul.size())
+        if(j>=m)
             return 0;
         
         if(dp[i][j]!=INT_MIN)
             return dp[i][j];
         
-        int t1=find(i+1,j+1,nums,mul)+mul[j]*nums[i];
-        int t2=find(i,j+1,nums,mul)+mul[j]*(nums[n-1-(j-i)]);
+        int left=find(i+1,j+1,nums,mul)+mul[j]*nums[i];
+        int right=find(i,j+1,nums,mul)+mul[j]*nums[n-1-(j-i)];
         
-        return dp[i][j]=max(t1,t2);
+        return dp[i][j]=max(left,right);
         
     }
     
     int maximumScore(vector<int>& nums, vector<int>& mul) {
-     int n=nums.size();   
+        int n=nums.size();
         int m=mul.size();
-        dp.resize(m+1,vector<int>(m+1,INT_MIN));
-        return find(0,0,nums,mul);
         
+       dp.resize(m+1,vector<int>(m+1,INT_MIN));
+        
+        return find(0,0,nums,mul);
         
     }
 };
