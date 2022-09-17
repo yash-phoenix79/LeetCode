6d7b43b71b35f99a2a81dec7bdc912d3ed455176
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-09-17 13:07:31 +0530

    Time: 46 ms (5.34%), Space: 11.4 MB (33.05%) - LeetHub

--- 1436-destination-city/1436-destination-city.cpp
+++ 1436-destination-city/1436-destination-city.cpp
@@ -1,29 +1,15 @@
 class Solution {
 public:
-    
-    string res;
-    
-    void dfs(string s,unordered_map<string,string>&m){
-        
-        if(m[s]=="")
-        {
-            res=s;
-            return;
-        }
-        
-        dfs(m[s],m);
-        
-    }
-    
     string destCity(vector<vector<string>>& paths) {
+        unordered_set<string>s;
         
-        unordered_map<string,string>m;
+        for(auto x:paths)
+            s.insert(x[0]);
         
         for(auto x:paths){
-            m[x[0]]=(x[1]);
+            if(!(s.count(x[1])))
+                return x[1];
         }
-        
-        dfs(paths[0][0],m);
-        return res;
+        return "";
     }
 };
