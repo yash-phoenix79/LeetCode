99c01c8790d0bd88a1a0271aaa2075bd2b7502ab
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-09-21 17:02:40 +0530

    Time: 279 ms (99.02%), Space: 114.8 MB (94.29%) - LeetHub

--- 1425-constrained-subsequence-sum/1425-constrained-subsequence-sum.cpp
+++ 1425-constrained-subsequence-sum/1425-constrained-subsequence-sum.cpp
@@ -4,17 +4,15 @@ public:
         int n=nums.size();
         deque<int>q;
         int ans=INT_MIN;
-        
         for(int i=0;i<n;i++){
-            while(!q.empty()&&q.front()+k<i)
-                q.pop_front();
+            while(!q.empty()&&q.front()+k<i)q.pop_front();
             int cur=nums[i]+(q.empty()?0:nums[q.front()]);
             while(!q.empty()&&nums[q.back()]<=cur)
                 q.pop_back();
             ans=max(ans,cur);
+            nums[i]=cur;
             if(cur>0)
                 q.push_back(i);
-            nums[i]=cur;
         }
         return ans;
     }
