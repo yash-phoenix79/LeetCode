87fa47bda859864e57557c51f6f5af083366d3a6
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-09-24 18:04:24 +0530

    Time: 511 ms (17.48%), Space: 66.8 MB (89.10%) - LeetHub

--- 2332-the-latest-time-to-catch-a-bus/2332-the-latest-time-to-catch-a-bus.cpp
+++ 2332-the-latest-time-to-catch-a-bus/2332-the-latest-time-to-catch-a-bus.cpp
@@ -6,23 +6,23 @@ public:
         
         int c;
         
-        unordered_set<int>s;
+      int prev=-1;
         sort(begin(bus),end(bus));
         sort(begin(pas),end(pas));
         int ans;
         for(i=0;i<n;i++){
             c=0;
             while(c<cap&&j<m&&bus[i]>=pas[j]){
-                if(s.find(pas[j]-1)==s.end())
+                if(prev!=pas[j]-1)
                 {
                     ans=pas[j]-1;
                    
                 }
-                 s.insert(pas[j]);
+                 prev=(pas[j]);
                 c++;
                 j++;
             }
-            if(c<cap&&s.find(bus[i])==s.end())
+            if(c<cap&&prev!=bus[i])
                 ans=bus[i];
         }
         return ans;
