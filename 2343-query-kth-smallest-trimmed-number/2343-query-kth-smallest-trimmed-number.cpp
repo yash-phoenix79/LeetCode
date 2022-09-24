ec35426da41e094fd45a8cae171b8c234b31298e
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-09-24 16:04:11 +0530

    Time: 2326 ms (28.20%), Space: 372.5 MB (65.20%) - LeetHub

--- 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.cpp
+++ 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.cpp
@@ -1,36 +1,17 @@
 class Solution {
 public:
-    
-    class myComparator{
-        public:
-        bool operator() (const pair<int,string> &a,const pair<int,string> &b){
-           
-            
-            if(a.second==b.second)return a.first<b.first;
-            return a.second<b.second;
-            
-        }
-    };
-    
     vector<int> smallestTrimmedNumbers(vector<string>& nums, vector<vector<int>>& q) {
-        
         int n=nums.size();
         int m=q.size(),len=nums[0].length();
         
-        vector<int>res(m);
-        
-        priority_queue<pair<int,string>,vector<pair<int,string>>,myComparator>pq,t;
+        vector<int>res;
         
         for(int i=0;i<m;i++){
-            int l=len-q[i][1];
-            t=pq;
-            for(int j=0;j<n;j++){
-                  string temp=nums[j].substr(l);
-                t.push({j,temp});
-                if(t.size()>q[i][0])
-                    t.pop();
-            }
-          res[i]=t.top().first;
+            vector<pair<string,int>>p;
+            for(int j=0;j<n;j++)
+                p.push_back({nums[j].substr(len-q[i][1]),j});
+            sort(p.begin(),p.end());
+            res.push_back(p[q[i][0]-1].second);
         }
         return res;
     }
