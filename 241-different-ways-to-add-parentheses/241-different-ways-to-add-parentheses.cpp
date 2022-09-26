1ce2b25dfa948bec9f20c5ffdd23615cb1674a7b
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-09-26 23:30:32 +0530

    Time: 6 ms (53.66%), Space: 11.6 MB (23.00%) - LeetHub

--- 241-different-ways-to-add-parentheses/241-different-ways-to-add-parentheses.cpp
+++ 241-different-ways-to-add-parentheses/241-different-ways-to-add-parentheses.cpp
@@ -1,30 +1,38 @@
 class Solution {
 public:
     vector<int> diffWaysToCompute(string exp) {
-        vector<int>ans;
+        
         int n=exp.length();
-        int check=1;
+        
+        int l=0,r=n-1;
+        vector<int>res;
+        
+        bool check=false;
         
         for(int i=0;i<n;i++){
             if(!isdigit(exp[i])){
-                check=0;
-                vector<int>l= diffWaysToCompute(exp.substr(0,i));
-                vector<int>r= diffWaysToCompute(exp.substr(i+1));
-                for(auto x:l){
-                    for(auto y:r){
-                        if(exp[i]=='+')
-                            ans.push_back(x+y);
-                        else if(exp[i]=='-')
-                            ans.push_back(x-y);
+                check=true;
+                vector<int>lt= diffWaysToCompute(exp.substr(0,i));
+                vector<int>rt= diffWaysToCompute(exp.substr(i+1));
+                
+                for(auto x:lt){
+                    for(auto y:rt){
+                        if(exp[i]=='-')
+                            res.push_back(x-y);
+                        else if(exp[i]=='+')
+                            res.push_back(x+y);
                         else
-                            ans.push_back(x*y);
+                            res.push_back(x*y);
                     }
                 }
                 
             }
         }
-        if(check)
-            return{stoi(exp)};
-        return ans;
+        
+        if(!check)
+            return {stoi(exp)};
+        return res;
+        
+        
     }
 };
