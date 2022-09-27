d78c7fa5cfd753e4ad8bacc6978cfebf2d2a6b1a
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-09-27 22:53:58 +0530

    Time: 39 ms (91.51%), Space: 17.2 MB (54.25%) - LeetHub

--- 838-push-dominoes/838-push-dominoes.cpp
+++ 838-push-dominoes/838-push-dominoes.cpp
@@ -1,54 +1,25 @@
 class Solution {
 public:
-    string pushDominoes(string dom) {
-        int n=dom.length();
-        string res(n,'.');
+    string pushDominoes(string d) {
+        d='L'+d+'R';
+        int n=d.length();
+        string res="";
         
-        vector<int>q;
-        
-        for(int i=0;i<n;i++){
-            if(dom[i]!='.'){
-                res[i]=dom[i];
-                q.push_back(i);
-            }
+        for(int i=0,j=1;j<n;j++){
+            if(d[j]=='.')continue;
+            
+            int midLen=j-i-1;
+            if(i>0)
+             res+=d[i];
+            
+            if(d[i]==d[j])
+                res+=string(midLen,d[i]);
+            else if(d[i]=='L'&&d[j]=='R')
+                res+=string(midLen,'.');
+            else
+                res+=string(midLen/2,'R')+string(midLen%2,'.')+string(midLen/2,'L');
+            i=j;
         }
-        
-        while(!q.empty()){
-            int m=q.size();
-            string temp=res;
-            vector<int>q1;
-            while(m--){
-                int cur=q.back();
-                // cout<<cur<<endl;
-                q.pop_back();
-                if(res[cur]=='R'){
-                    if(cur+1==n||(cur+1<n&&res[cur+1]!='.'))
-                        continue;
-                    if(cur+2<n&&res[cur+2]=='L')
-                        continue;
-                    else{
-                   temp[cur+1]='R';
-                    q1.push_back(cur+1); 
-                    }
-
-                }
-                else {
-                    if(cur-1<0||(cur-1>=0&&res[cur-1]!='.'))
-                        continue;
-                    if(cur-2>=0&&res[cur-2]=='R')
-                        continue;
-                    else{
-                      temp[cur-1]='L';
-                    q1.push_back(cur-1);  
-                    }
-                    
-                }
-            }
-            res=temp;
-            q=q1;
-        }
-        
         return res;
-        
     }
 };
