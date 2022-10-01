07333ffc3d3890881e55d074213e72ad57781c75
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-10-01 18:15:59 +0530

    Time: 39 ms (17.63%), Space: 5.9 MB (74.02%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        
+        if(x<0)
+            return false;
+        string s=to_string(x);
+        string t=s;
+        reverse(t.begin(),t.end());
+        return t==s;
+        
+    }
+};
