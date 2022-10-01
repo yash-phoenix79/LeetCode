db4c170f820ba54b509c35609836dab4b493d1d5
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-10-01 18:14:07 +0530

    Time: 27 ms (57.40%), Space: 11 MB (32.81%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> twoSum(vector<int>& nums, int tar) {
+        
         int n=nums.size();
         unordered_map<int,int>m;
-        vector<int>ans;
-        
         for(int i=0;i<n;i++){
-            if(m.find(nums[i])!=m.end())
-            {
-                ans={m[nums[i]],i};
-                return ans;
+            if(m.find(nums[i])!=m.end()){
+                return {i,m[nums[i]]};
             }
-            m[target-nums[i]]=i;
+            m[tar-nums[i]]=i;
         }
-        return ans;
+        return {};
     }
 };
