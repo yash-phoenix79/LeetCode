a745de6119bd429623340b9e2d9801fedac3c8ca
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-10-01 17:16:29 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (49.09%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
-    
-    int find(int idx,string s,vector<int>&dp,int n){
-        if(idx==n)
-            return 1;
-        if(dp[idx]!=-1)
-            return dp[idx];
-        if(s[idx]=='0')
-            return dp[idx]=0;
-        if(idx<n-1&&(s[idx]=='1'||(s[idx]=='2'&&(s[idx+1]>='0'&&s[idx+1]<='6'))))
-            return dp[idx]=find(idx+1,s,dp,n)+find(idx+2,s,dp,n);
-        return dp[idx]=find(idx+1,s,dp,n);
-    }
-    
     int numDecodings(string s) {
         int n=s.length();
-        vector<int>dp(n,-1);
-        return find(0,s,dp,n);
+		if(s[0]=='0')
+			return 0;
+        vector<int> dp(n+1,1);
+        for(int i=n-1;i>=0;i--){
+            if(s[i]=='0')
+                dp[i]=0;
+            else if(i!=n-1&&(s[i]=='1'||(s[i]=='2'&&s[i+1]<='6')))
+                dp[i]=dp[i+1]+dp[i+2];
+            else
+                dp[i]=dp[i+1];
+        }
+        return dp[0];
     }
 };
