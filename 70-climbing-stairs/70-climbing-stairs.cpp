d61358e7fe8f42fbdc24d29ea1856e60ded981d6
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-10-02 16:08:17 +0530

    Time: 6 ms (6.16%), Space: 5.9 MB (56.91%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int climbStairs(int n) {
-     
+        
         if(n==1)
             return 1;
         if(n==2)
             return 2;
         
-        int fir=1,sec=2;
-        
-        int ans;
-        
-        for(int i=0;i<n-2;i++){
-            ans=fir+sec;
-            fir=sec;
-            sec=ans;
+        int f=1,s=2,t;
+        n-=2;
+        while(n--){
+            t=f+s;
+            f=s;
+            s=t;
+            
         }
-        return ans;
+        return t;
+        
     }
 };
