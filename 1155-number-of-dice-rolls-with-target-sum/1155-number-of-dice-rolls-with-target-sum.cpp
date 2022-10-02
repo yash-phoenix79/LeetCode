0e4bd1f55f40bd55783c44145c596b00585f65fb
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-10-02 15:38:19 +0530

    Time: 215 ms (5.67%), Space: 8.5 MB (28.55%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,40 +1,21 @@
 class Solution {
 public:
+    
     #define N 1000000007
-   
     
-    vector<vector<long long>>dp;
-    
-    long long find(int idx,int n,int k,int t){
+    int numRollsToTarget(int n, int k, int t) {
         
-        if(t<0)
-            return 0;
-        if(t==0&&idx!=n)
-            return 0;
-        if(idx==n){
-            if(t==0)
-                return 1;
-            return 0;
+        vector<vector<int>>dp(n+1,vector<int>(t+1,0));
+        dp[0][0]=1;
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=t;j++){
+                for(int l=1;l<=k;l++){
+                    if(l<=j)
+                    dp[i][j]=(dp[i][j]%N+dp[i-1][j-l]%N)%N;
+                }
+            }
         }
-        
-        long long res=0;
-        
-        if(dp[idx][t]!=-1)
-            return dp[idx][t];
-        
-        for(int i=1;i<=k;i++){
-            res=(res%N+find(idx+1,n,k,t-i)%N)%N;
-        }
-        
-        return dp[idx][t]=res%N;
-        
-    }
-    
-    int numRollsToTarget(int n, int k, int tar) {
-           
-        
-        dp=vector<vector<long long>>(n+1,vector<long long>(tar+1,-1));
-        
-        return (int)find(0,n,k,tar)%N;
+        return dp[n][t];
     }
 };
