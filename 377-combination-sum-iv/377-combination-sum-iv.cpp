8d9aa414f71633fced05f76be4626b0edfad923b
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-10-08 18:46:10 +0530

    Time: 7 ms (21.52%), Space: 6.7 MB (67.39%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-   
     
-    int find(vector<int>&nums,int n,int t,vector<int>&dp){
+    vector<int>dp;
+    
+    int find(vector<int>&nums,int n,int t){
+        
         if(t==0)
-        {
-           return 1;
-        }
+            return 1;
         
         if(dp[t]!=-1)
             return dp[t];
         int res=0;
         for(int i=0;i<n;i++){
             if(nums[i]<=t)
-                res+=find(nums,n,t-nums[i],dp);
+                res+=find(nums,n,t-nums[i]);
         }
         return dp[t]=res;
     }
     
-    int combinationSum4(vector<int>& nums, int tar) {
-        int n=nums.size();
-        vector<int>dp(tar+1,-1);
+    int combinationSum4(vector<int>& nums, int t) {
+        
+        dp.resize(t+1,-1);
         dp[0]=0;
-       return find(nums,n,tar,dp);
+        int n=nums.size();
+        return find(nums,n,t);
         
     }
 };
