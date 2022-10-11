7f282d5ca5c95e77b0fd8c965996557456439fba
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-10-11 22:53:54 +0530

    Time: 158 ms (31.79%), Space: 23.7 MB (75.81%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     int countSquares(vector<vector<int>>& mat) {
-     
-        
-        for(int i=1;i<mat.size();i++){
-            for(int j=1;j<mat[0].size();j++){
-                if(mat[i][j]==0)continue;
-                mat[i][j]=1+min({mat[i-1][j],mat[i][j-1],mat[i-1][j-1]});
-            }
-        }
         
         int res=0;
-        for(auto x:mat){
-            for(auto y:x)
-                res+=y;
+        
+        for(int i=0;i<mat.size();i++){
+            for(int j=0;j<mat[0].size();j++){
+                if(i==0||j==0){
+                    res+=mat[i][j];
+                }
+                else if(mat[i][j]==0)continue;
+                else{
+                    mat[i][j]=1+min({mat[i][j-1],mat[i-1][j],mat[i-1][j-1]});
+                res+=(mat[i][j]);
+                }
+                
+            }
         }
-        
         return res;
-        
     }
 };
