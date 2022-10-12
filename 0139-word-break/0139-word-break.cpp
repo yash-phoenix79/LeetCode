893078ccd4aba48439457718346a215920db6e95
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-10-12 16:56:42 +0530

    Time: 32 ms (36.24%), Space: 13 MB (46.28%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& w) {
+        
         int n=s.length();
+        
         vector<bool>dp(n+1,false);
         dp[0]=true;
         
         for(int i=1;i<=n;i++){
             for(int j=0;j<i;j++){
-                if(dp[j]&&find(w.begin(),w.end(),s.substr(j,i-j))!=w.end()){
+                if(dp[j]&&find(w.begin(),w.end(),s.substr(j,i-j))!=w.end())
+                {
                     dp[i]=true;
                     break;
                 }
             }
         }
-        return dp.back();
+        return dp[n];
+        
     }
 };
