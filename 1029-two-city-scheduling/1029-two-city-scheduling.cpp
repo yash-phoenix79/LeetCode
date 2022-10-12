c654fcc1c70dffe9fdfd721f02cb476cc2e84a56
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-10-12 23:44:12 +0530

    Time: 3 ms (95.47%), Space: 7.9 MB (45.65%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -1,39 +1,17 @@
 class Solution {
 public:
-   vector<vector<int>>dp;
-    
-    int find(int idx,int ac,int bc,vector<vector<int>>& costs){
-        
-        int n=costs.size();
-        if(idx>=n)return 0;
-        
-        if(dp[ac][bc]!=-1)
-            return dp[ac][bc];
-        
-        int t=n/2;
-        
-     int ans1=INT_MAX,ans2=INT_MAX;
-        
-        if(ac<t)
-             ans1=costs[idx][0]+find(idx+1,ac+1,bc,costs);
-        
-        
-        if(bc<t)
-             ans2=costs[idx][1]+find(idx+1,ac,bc+1,costs);
-        
-        return dp[ac][bc]=min(ans1,ans2);
-        
-    }
-    
     int twoCitySchedCost(vector<vector<int>>& costs) {
-        
+     
         int n=costs.size();
-        int t=n/2;
+        sort(begin(costs),end(costs),[](auto &a,auto &b){
+           return a[0]-a[1]<b[0]-b[1]; 
+        });
         
-        dp=vector<vector<int>>(t+2,vector<int>(t+2,-1));
-        
-        int ac=0,bc=0;
-        return find(0,ac,bc,costs);
-        
+        int res=0;
+        for(int i=0;i<n/2;i++){
+            res+=costs[i][0];
+            res+=costs[i+n/2][1];
+        }
+        return res;
     }
 };
