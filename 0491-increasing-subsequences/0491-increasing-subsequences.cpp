a1dce25b8db0de9518b7e6d30e6ce313b1423948
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-10-17 10:51:54 +0530

    Time: 127 ms (54.63%), Space: 26.5 MB (44.35%) - LeetHub

--- 0491-increasing-subsequences/0491-increasing-subsequences.cpp
+++ 0491-increasing-subsequences/0491-increasing-subsequences.cpp
@@ -1,40 +1,29 @@
 class Solution {
 public:
-   set<vector<int>>res;
-    vector<vector<int>>ans;
     
-    void find(int i,vector<int>&nums,vector<int>sol){
-        int n=nums.size();
-        if(i>=n){
-            if(sol.size()>1){
-               
-                    res.insert(sol);
-            }
-                
-            return;
-        }
-            
-        if(sol.empty()||sol.back()<=nums[i]){
-            find(i+1,nums,sol);
-            sol.push_back(nums[i]);
-            find(i+1,nums,sol);
-            sol.pop_back();
-        }
+    void find(int idx,vector<int>& nums,vector<vector<int>>&res,vector<int>sol){
+             int n=nums.size();
+        if(sol.size()>1)
+            res.push_back(sol);
+        unordered_set<int>s;
         
-        else
-            find(i+1,nums,sol);
+        for(int i=idx;i<n;i++){
+            if((sol.empty()||sol.back()<=nums[i])&&s.find(nums[i])==s.end()){
+                sol.push_back(nums[i]);
+                find(i+1,nums,res,sol);
+                sol.pop_back();
+                s.insert(nums[i]);
+            }
+        }
         
     }
     
     vector<vector<int>> findSubsequences(vector<int>& nums) {
-        
         int n=nums.size();
+        vector<vector<int>>res;
         vector<int>sol;
-        find(0,nums,sol);
         
-        for(auto x:res)
-            ans.push_back(x);
-        
-        return ans;
+        find(0,nums,res,sol);
+        return res;
     }
 };
