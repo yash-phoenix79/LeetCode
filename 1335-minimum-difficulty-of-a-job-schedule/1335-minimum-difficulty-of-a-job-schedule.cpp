3ac180e36d3be598c180cd350772d334e088d945
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-10-17 09:56:55 +0530

    Time: 131 ms (34.54%), Space: 7.4 MB (82.89%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
@@ -3,17 +3,19 @@ int dp[301][11];
 class Solution {
 public:
     
-    int find(int idx,int n,vector<int>& j,int d){
-        
-        if(d==1)
+    int find(int idx,vector<int>& j,int n,int d){
+        if(d==1){
             return *max_element(j.begin()+idx,j.end());
+        }
+        
         if(dp[idx][d]!=-1)
-            return dp[idx][d];
+        return dp[idx][d];
         int val=INT_MIN;
         int res=INT_MAX;
+        
         for(int i=idx;i<=n-d;i++){
             val=max(val,j[i]);
-            res=min(res,val+find(i+1,n,j,d-1));
+            res=min(res,val+find(i+1,j,n,d-1));
         }
         return dp[idx][d]=res;
     }
@@ -23,6 +25,6 @@ public:
         if(n<d)
             return -1;
         memset(dp,-1,sizeof(dp));
-        return find(0,n,j,d);
+        return find(0,j,n,d);
     }
 };
