f81610231803fdeb19d65030d2ebd3f3e9e16ac7
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-10-18 20:30:14 +0530

    Time: 13 ms (45.27%), Space: 8.9 MB (11.62%) - LeetHub

--- 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.cpp
+++ 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.cpp
@@ -1,50 +1,50 @@
 class Solution {
 public:
     
-    int f,s;
-    vector<int>pref;
-    
-    vector<vector<vector<int>>>dp;
-    
-    int find(int idx,vector<int>& nums,int fir,int sec){
-        
+    int find(vector<int>&nums,int f,int s){
         int n=nums.size();
-        if(idx>=n)
-            return 0;
-        int res=INT_MIN;
         
-        if(dp[idx][fir][sec]!=-1)
-            return dp[idx][fir][sec];
+        vector<int>dp1(n,0),dp2(n,0);
         
-        res=max(res,find(idx+1,nums,fir,sec));
+        int sum=0;
         
-        if(idx+f<=n&&fir){
-            res=max(res,pref[idx+f]-pref[idx]+find(idx+f,nums,!fir,sec));
+        for(int i=0;i<n;i++){
+            if(i<f)
+            {
+                sum+=nums[i];
+                dp1[i]=sum;
+            }
+            else
+            {
+                sum=sum+nums[i]-nums[i-f];
+                dp1[i]=max(dp1[i-1],sum);
+            }
         }
         
-        if(idx+s<=n&&sec)
-            res=max(res,pref[idx+s]-pref[idx]+find(idx+s,nums,fir,!sec));
+        sum=0;
         
-        return dp[idx][fir][sec]=res;
+        for(int i=n-1;i>=0;i--){
+            if(i+s>n-1){
+                sum+=nums[i];
+                dp2[i]=sum;
+            }
+            else{
+                sum=sum+nums[i]-nums[i+s];
+                dp2[i]=max(dp2[i+1],sum);
+            }
+        }
+        
+        int res=0;
+        for(int i=f-1;i+s<n;i++)
+            res=max(res,dp1[i]+dp2[i+1]);
+        
+        return res;
+        
     }
     
-    int maxSumTwoNoOverlap(vector<int>& nums, int first, int second) {
+    int maxSumTwoNoOverlap(vector<int>& nums, int firstLen, int secondLen) {
         
-        int n=nums.size();
-        pref=vector<int>(n+1,0);
-        pref[0]=0;
-        for(int i=1;i<=n;i++)
-            pref[i]=pref[i-1]+nums[i-1];
-        
-        // for(auto x:pref)
-        //     cout<<x<<" ";
-        
-        f=first;
-        s=second;
-        
-        dp=vector<vector<vector<int>>>(n+1,vector<vector<int>>(2,vector<int>(2,-1)));
-        
-        return find(0,nums,1,1);
+        return max(find(nums,firstLen,secondLen),find(nums,secondLen,firstLen));
         
     }
 };
