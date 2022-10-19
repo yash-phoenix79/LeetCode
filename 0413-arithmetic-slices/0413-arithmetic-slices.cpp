df2873c3f113c13e4c5c18e41083a5e34db11629
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-10-19 16:53:49 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (85.34%) - LeetHub

--- 0413-arithmetic-slices/0413-arithmetic-slices.cpp
+++ 0413-arithmetic-slices/0413-arithmetic-slices.cpp
@@ -3,17 +3,21 @@ public:
     int numberOfArithmeticSlices(vector<int>& nums) {
         
         int n=nums.size();
-        int cnt=0,diff;
+        if(n<3)
+        return 0;
+        int res=0,pre=nums[1]-nums[0],ind=0;
         
-        for(int i=0;i<n-2;i++){
-            diff=nums[i+1]-nums[i];
-            for(int j=i+2;j<n;j++){
-                if(nums[j]-nums[j-1]==diff)
-                    cnt++;
-                else
-                    break;
+        for(int i=1;i<n-1;i++){
+            
+            if(nums[i+1]-nums[i]==pre){
+                ind++;
             }
+            else{
+                pre=nums[i+1]-nums[i];
+                ind=0;
+            }
+            res+=ind;
         }
-        return cnt;
+        return res;
     }
 };
