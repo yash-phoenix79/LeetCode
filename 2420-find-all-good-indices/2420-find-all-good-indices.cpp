c427b8152435f1a3180004112c0ba4d2bab7885f
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-10-20 15:50:25 +0530

    Time: 209 ms (78.16%), Space: 89.1 MB (13.76%) - LeetHub

--- 2420-find-all-good-indices/2420-find-all-good-indices.cpp
+++ 2420-find-all-good-indices/2420-find-all-good-indices.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
     vector<int> goodIndices(vector<int>& nums, int k) {
+        
         vector<int>res;
         int n=nums.size();
         vector<int>dp1(n,1),dp2(n,1);
         
         for(int i=1;i<n;i++)
-            if(nums[i]<=nums[i-1])dp1[i]=1+dp1[i-1];
-        for(int i=n-2;i>=0;i--)
-            if(nums[i]<=nums[i+1])dp2[i]=1+dp2[i+1];
-        for(int i=k;i<n-k;i++)
-            if(dp1[i-1]>=k&&dp2[i+1]>=k)res.push_back(i);
-        return res;
+            if(nums[i]<=nums[i-1])
+                dp1[i]=1+dp1[i-1];
         
+        for(int i=n-2;i>=0;i--)
+            if(nums[i]<=nums[i+1])
+                dp2[i]=1+dp2[i+1];
+        
+        for(int i=1;i<n-1;i++){
+            if(dp1[i-1]>=k&&dp2[i+1]>=k)res.push_back(i);
+        }
+        return res;
     }
 };
