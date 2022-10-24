6f09f772865439a9726a9c1624b6f8c92309bbab
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-10-24 14:33:52 +0530

    Time: 9 ms (88.33%), Space: 8.1 MB (92.84%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -3,30 +3,31 @@ public:
     
     int res=0;
     
-    void find(int idx,vector<string>& arr,int n,vector<int>al,int len){
+    void find(int idx,vector<string>& arr,int n,int mask,int len){
         
         res=max(res,len);
         if(idx==n)return;
         
-        find(idx+1,arr,n,al,len);
+        find(idx+1,arr,n,mask,len);
         
         
         int dis=arr[idx].size();
         for(int i=0;i<dis;i++){
             
-            if(al[arr[idx][i]-'a']!=0)
+           int t=arr[idx][i]-'a';
+            if(mask&(1<<t))
                 return;
-            al[arr[idx][i]-'a']++;
+            mask=mask|(1<<t);
         }
         
-        find(idx+1,arr,n,al,len+arr[idx].size());
+        find(idx+1,arr,n,mask,len+arr[idx].size());
         
     }
     
     int maxLength(vector<string>& arr) {
         int n=arr.size();
-        vector<int>al(26,0);
-        find(0,arr,n,al,0);
+        int mask=0;
+        find(0,arr,n,mask,0);
         return res;
     }
 };
