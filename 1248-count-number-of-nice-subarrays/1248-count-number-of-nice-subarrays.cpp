fb9d3587c474ed3e10fbb999f8bc3e120b064c0a
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-10-25 08:52:41 +0530

    Time: 295 ms (63.10%), Space: 67.6 MB (75.94%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
     
-    int atmost(vector<int>& nums, int k){
-        int res=0,i=0,j;
+    int atmost(vector<int>&nums,int k){
+        
         int n=nums.size();
-        for(j=0;j<n;j++){
+        int i=0,j=0;
+        int res=0;
+        
+        for(;j<n;j++){
             k-=nums[j]%2;
             while(k<0)
                 k+=nums[i++]%2;
-            res+=j-i+1;
+            res+=(j-i+1);
         }
         return res;
     }
     
     int numberOfSubarrays(vector<int>& nums, int k) {
+        
+        int n=nums.size();
         return atmost(nums,k)-atmost(nums,k-1);
+        
     }
 };
