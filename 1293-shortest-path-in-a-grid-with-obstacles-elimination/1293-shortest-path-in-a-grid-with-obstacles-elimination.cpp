ed502bcc244d1aaa0cae3b4b183194c773230de2
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-11-01 18:45:11 +0530

    Time: 434 ms (23.27%), Space: 50.7 MB (34.30%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -6,7 +6,6 @@ public:
         int m=grid[0].size();
         
         vector<vector<int>>vis(n,vector<int>(m,-1));
-        
         queue<vector<int>>q;
         q.push({0,0,0,k});
         
@@ -17,23 +16,22 @@ public:
             
             int x=t[0],y=t[1];
             
-            if(x<0||y<0||x>=n||y>=m)continue;
+            if(x<0||y<0||x>=n||y>=m)
+                continue;
             
             if(x==n-1&&y==m-1)
-            {
                 return t[2];
-            }
             
-            if(grid[x][y]==1){
-                if(t[3]>0)
+            if(grid[x][y]){
+                if(t[3]>0){
                     t[3]--;
+                }
                 else
                     continue;
             }
             
             if(vis[x][y]!=-1&&vis[x][y]>=t[3])
                 continue;
-            
             vis[x][y]=t[3];
             
             q.push({x+1,y,t[2]+1,t[3]});
