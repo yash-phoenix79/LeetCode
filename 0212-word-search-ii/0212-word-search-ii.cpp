7dc0adbac876634b024b88f89b342473d6ca108e
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-11-06 20:02:17 +0530

    Time: 818 ms (73.91%), Space: 15.6 MB (24.59%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.cpp
+++ 0212-word-search-ii/0212-word-search-ii.cpp
@@ -1,59 +1,67 @@
 class Solution {
-    struct TrieNode {
-        TrieNode *children[26];
-        string word;
-
-        TrieNode() : word("") {
-            for (int i = 0; i < 26; i++) {
-                children[i] = nullptr;
-            }
-        }
-    };
-
 public:
-    vector<string> findWords(vector<vector<char>> &board, vector<string> &words) {
-        TrieNode *root = buildTrie(words);
-        vector<string> result;
-        for (int i = 0; i < board.size(); i++) {
-            for (int j = 0; j < board[0].size(); j++) {
-                dfs(board, i, j, root, result);
-            }
+    
+    vector<string>res;
+    
+    class TrieNode{
+        public:
+        string word;
+        TrieNode* child[26];
+        TrieNode(){
+            word="";
+            for(int i=0;i<26;i++)
+                child[i]=NULL;
         }
-        return result;
-    }
-
-    /** Inserts a word into the trie. */
-    TrieNode *buildTrie(vector<string> &words) {
-        TrieNode *root = new TrieNode();
-        for (int j = 0; j < words.size(); j++) {
-            string word = words[j];
-            TrieNode *curr = root;
-            for (int i = 0; i < word.length(); i++) {
-                char c = word[i] - 'a';
-                if (curr->children[c] == nullptr) {
-                    curr->children[c] = new TrieNode();
-                }
-                curr = curr->children[c];
+        
+    };
+    
+    
+    
+    
+    TrieNode* insert(vector<string>&words){
+        TrieNode* root=new TrieNode();
+        for(int i=0;i<words.size();i++){
+            TrieNode* cur=root;
+            for(int j=0;j<words[i].length();j++){
+                int k=words[i][j]-'a';
+                if(!cur->child[k])
+                    cur->child[k]=new TrieNode();
+                cur=cur->child[k];
             }
-            curr->word = word;
+            cur->word=words[i];
         }
         return root;
     }
-
-    void dfs(vector<vector<char>> &board, int i, int j, TrieNode *p, vector<string> &result) {
-        char c = board[i][j];
-        if (c == '#' || !p->children[c - 'a']) return;
-        p = p->children[c - 'a'];
-        if (p->word.size() > 0) {
-            result.push_back(p->word);
-            p->word = "";
+    
+    void dfs(int x,int y,vector<vector<char>>& b,TrieNode* p){
+        char c=b[x][y];
+        if(c=='#'||!p->child[c-'a'])return;
+        p=p->child[c-'a'];
+        
+        if(p->word.size()>0){
+            res.push_back(p->word);
+            p->word="";
         }
-
-        board[i][j] = '#';
-        if (i > 0) dfs(board, i - 1, j, p, result);
-        if (j > 0) dfs(board, i, j - 1, p, result);
-        if (i < board.size() - 1) dfs(board, i + 1, j, p, result);
-        if (j < board[0].size() - 1) dfs(board, i, j + 1, p, result);
-        board[i][j] = c;
+        
+        b[x][y]='#';
+        if(x>0)dfs(x-1,y,b,p);
+        if(y>0)dfs(x,y-1,b,p);
+        if(x<b.size()-1)dfs(x+1,y,b,p);
+        if(y<b[0].size()-1)dfs(x,y+1,b,p);
+        
+        b[x][y]=c;
+        
+    }
+    
+    vector<string> findWords(vector<vector<char>>& b, vector<string>& words) {
+        
+        TrieNode* root=insert(words);    
+        
+        for(int i=0;i<b.size();i++){
+            for(int j=0;j<b[0].size();j++){
+                dfs(i,j,b,root);
+            }
+        }
+        return res;
     }
 };
