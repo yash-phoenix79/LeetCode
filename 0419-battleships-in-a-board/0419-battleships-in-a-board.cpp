ff95dea53c41d8bdff0567c0696f82223adad853
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-11-07 20:03:55 +0530

    Time: 20 ms (36.34%), Space: 8.1 MB (85.15%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.cpp
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
-    
-    void dfs(int x,int y,vector<vector<char>>& b){
-        int n=b.size(),m=b[0].size();
-        b[x][y]='.';
-        
-        // cout<<x<<" "<<y<<endl;
-        
-        if(x>0&&b[x-1][y]=='X')dfs(x-1,y,b);
-        if(y>0&&b[x][y-1]=='X')dfs(x,y-1,b);
-        if(x<n-1&&b[x+1][y]=='X')dfs(x+1,y,b);
-        if(y<m-1&&b[x][y+1]=='X')dfs(x,y+1,b);
-        
-    }
-    
     int countBattleships(vector<vector<char>>& b) {
         
-        int n=b.size(),m=b[0].size(),res=0;
+        int n=b.size();
+        int m=b[0].size();
+        
+        int res=0;
         
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(b[i][j]=='X')
-                {
+                
+                if(b[i][j]=='X'){
+                    
+                    if(i-1>=0&&b[i-1][j]=='X')continue;
+                    if(j-1>=0&&b[i][j-1]=='X')continue;
                     res++;
-                    dfs(i,j,b);
                 }
+                
             }
         }
         
