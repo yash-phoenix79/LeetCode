d5aa964c94b7687a557dbbd182f3a7de98440a2d
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-11-08 16:59:26 +0530

    Time: 370 ms (41.36%), Space: 56.9 MB (45.34%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.cpp
+++ 0310-minimum-height-trees/0310-minimum-height-trees.cpp
@@ -1,92 +1,48 @@
 class Solution {
 public:
-    
-    int l;
-    
-    vector<int> find(int v,vector<vector<int>>&adj){
-        int n=adj.size();
-        vector<int>vis(n,-1);
-        vis[v]=0;
-        
-        queue<int>q;
-        q.push(v);
-        
-        int t=1;
-        while(!q.empty()){
-            int sz=q.size();
-            while(sz--){
-                int x=q.front();
-                q.pop();
-                l=x;
-                for(auto a:adj[x]){
-                    if(vis[a]==-1){
-                        vis[a]=t;
-                        q.push(a);
-                    }
-                }
-            }
-            t++;
-        }
-        
-//         cout<<l<<" ";
-        
-        return vis;
-        
-    }
-    
     vector<int> findMinHeightTrees(int n, vector<vector<int>>& e) {
-       
         
         vector<vector<int>>adj(n);
+        
+        vector<int>inD(n),res;
+        
         for(auto x:e){
             adj[x[0]].push_back(x[1]);
             adj[x[1]].push_back(x[0]);
+            inD[x[0]]++;
+            inD[x[1]]++;
         }
         
-     
         queue<int>q;
-        q.push(0);
-        vector<int>vis(n,0);
-        vis[0]=1;
+        
+       for(int i=0;i<n;i++){
+           if(inD[i]==1){
+               q.push(i);
+               inD[i]=0;
+           }
+       }
         
         while(!q.empty()){
-            int v=q.front();
-            q.pop();
-            l=v;
-            for(auto x:adj[v]){
-                if(!vis[x]){
-                    vis[x]=1;
-                    q.push(x);
+            
+            int sz=q.size();
+            res.clear();
+            while(sz--){
+                int v=q.front();
+                q.pop();
+                res.push_back(v);
+                for(auto x:adj[v]){
+                    inD[x]--;
+                    if(inD[x]==1)
+                        q.push(x);
                 }
             }
-        }
-        
-        // cout<<l<<endl;
-        
-        vector<int>f=find(l,adj);
-        vector<int>s=find(l,adj);
-        
-        vector<int>res(n);
-        
-        for(int i=0;i<n;i++)
-            res[i]=max(f[i],s[i]);
-        
-        int te=INT_MAX;
-        vector<int>ans;
-        for(int i=0;i<n;i++){
-            if(res[i]<te){
-                te=res[i];
-                ans.clear();
-                ans.push_back(i);
-            }
-            else if(res[i]==te)
-                ans.push_back(i);
-            
-            // cout<<res[i]<<" ";
             
         }
         
-        return ans;
+       
+      if(n==1)return {0};
+        
+        return res;
         
     }
 };
