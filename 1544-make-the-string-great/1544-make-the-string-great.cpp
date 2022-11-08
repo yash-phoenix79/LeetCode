5a6ad57ed5e0f316b4232606359da15689bc74d4
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-11-08 16:00:36 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (82.16%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     string makeGood(string s) {
-        int n=s.length(),p=0;
         
-        for(int i=0;i<s.length();i++){
+        int n=s.length(),p=0;
+        for(int i=0;i<n;i++){
             if(p>0&&abs(s[i]-s[p-1])==32)
-                --p;
+                p--;
             else
                 s[p++]=s[i];
         }
