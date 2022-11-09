b563437e33df2fbe0b0a48cd138babc987eb1c8d
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-11-09 19:18:08 +0530

    Time: 27 ms (87.04%), Space: 11.6 MB (22.63%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -5,46 +5,47 @@ public:
         long long res=0;
         int n=nums.size();
         
-        vector<int>lmin(n,-1),lmax(n,-1),rmin(n,n),rmax(n,n);
-        vector<int>s;
+        vector<int>lmini(n,-1),rmini(n,n),lmaxi(n,-1),rmaxi(n,n);
+        vector<int>t;
         
         for(int i=0;i<n;i++){
-            while(s.size()&&nums[i]<=nums[s.back()])s.pop_back();
-            if(s.size())lmin[i]=s.back();
-            s.push_back(i);
+            while(!t.empty()&&nums[i]<=nums[t.back()])t.pop_back();
+            if(!t.empty())lmini[i]=t.back();
+            t.push_back(i);
+        }
+        
+        t.clear();
+        
+        for(int i=0;i<n;i++){
+            while(!t.empty()&&nums[i]>=nums[t.back()])t.pop_back();
+            if(!t.empty())lmaxi[i]=t.back();
+            t.push_back(i);
+        }
+        
+        t.clear();
+        
+        for(int i=n-1;i>=0;i--){
+            while(!t.empty()&&nums[i]<nums[t.back()])t.pop_back();
+            if(!t.empty())rmini[i]=t.back();
+            t.push_back(i);
+        }
+        
+        t.clear();
+        
+        for(int i=n-1;i>=0;i--){
+            while(!t.empty()&&nums[i]>nums[t.back()])t.pop_back();
+            if(!t.empty())rmaxi[i]=t.back();
+            t.push_back(i);
+        }
+        
+        for(int i=0;i<n;i++){
+            long long lmx=i-lmaxi[i];
+            long long rmx=rmaxi[i]-i;
+            long long lmn=i-lmini[i];
+            long long rmn=rmini[i]-i;
             
-        }
-        
-        s.clear();
-        
-        for(int i=n-1;i>=0;i--){
-            while(s.size()&&nums[i]<nums[s.back()])s.pop_back();
-            if(s.size())rmin[i]=s.back();
-            s.push_back(i);
-        }
-        
-        s.clear();
-        
-        for(int i=0;i<n;i++){
-            while(s.size()&&nums[i]>=nums[s.back()])s.pop_back();
-            if(s.size())lmax[i]=s.back();
-            s.push_back(i);
-        }
-        
-        s.clear();
-        
-        for(int i=n-1;i>=0;i--){
-            while(s.size()&&nums[i]>nums[s.back()])s.pop_back();
-            if(s.size())rmax[i]=s.back();
-            s.push_back(i);
-        }
-        
-        for(int i=0;i<n;i++){
-            long long leftMin=i-lmin[i];
-            long long leftMax=i-lmax[i];
-            long long rightMin=rmin[i]-i;
-            long long rightMax=rmax[i]-i;
-            res+=((leftMax*rightMax)-(leftMin*rightMin))*nums[i];
+            res+=(lmx*rmx-lmn*rmn)*nums[i];
+            
         }
         return res;
     }
