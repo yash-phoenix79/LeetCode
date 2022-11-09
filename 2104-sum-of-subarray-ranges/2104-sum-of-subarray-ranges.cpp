64e70053e9340a1c334045af3140b8daae6ad1d9
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-11-09 18:50:13 +0530

    Time: 31 ms (83.56%), Space: 11.5 MB (22.63%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -1,21 +1,51 @@
 class Solution {
 public:
     long long subArrayRanges(vector<int>& nums) {
+        
         long long res=0;
         int n=nums.size();
         
+        vector<int>lmin(n,-1),lmax(n,-1),rmin(n,n),rmax(n,n);
+        vector<int>s;
+        
         for(int i=0;i<n;i++){
-            int mi=INT_MAX;
-            int ma=INT_MIN;
-            for(int j=i;j<n;j++){
-                mi=min(mi,nums[j]);
-                ma=max(ma,nums[j]);
-                res+=(long long)(ma-mi);
-                // cout<<nums[j]<<" ";
-            }
-            // cout<<endl;
+            while(s.size()&&nums[i]<=nums[s.back()])s.pop_back();
+            if(s.size())lmin[i]=s.back();
+            s.push_back(i);
+            
+        }
+        
+        s.clear();
+        
+        for(int i=n-1;i>=0;i--){
+            while(s.size()&&nums[i]<nums[s.back()])s.pop_back();
+            if(s.size())rmin[i]=s.back();
+            s.push_back(i);
+        }
+        
+        s.clear();
+        
+        for(int i=0;i<n;i++){
+            while(s.size()&&nums[i]>=nums[s.back()])s.pop_back();
+            if(s.size())lmax[i]=s.back();
+            s.push_back(i);
+        }
+        
+        s.clear();
+        
+        for(int i=n-1;i>=0;i--){
+            while(s.size()&&nums[i]>nums[s.back()])s.pop_back();
+            if(s.size())rmax[i]=s.back();
+            s.push_back(i);
+        }
+        
+        for(int i=0;i<n;i++){
+            long long leftMin=i-lmin[i];
+            long long leftMax=i-lmax[i];
+            long long rightMin=rmin[i]-i;
+            long long rightMax=rmax[i]-i;
+            res+=((leftMax*rightMax)-(leftMin*rightMin))*nums[i];
         }
         return res;
-        
     }
 };
