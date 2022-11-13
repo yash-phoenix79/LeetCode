9fbae5287adca2c47619d5519d4b3675a34d4e0c
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-11-13 15:19:21 +0530

    Time: 1337 ms (47.01%), Space: 249 MB (29.20%) - LeetHub

--- 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.cpp
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.cpp
@@ -1,56 +1,50 @@
 class Solution {
 public:
     
-    void merge(vector<pair<int,int>>&arr,int l,int m,int r,vector<int>&cnt){
+    void merge(vector<pair<int,int>>&a,vector<int>&cnt,int l,int m,int r){
         
         vector<pair<int,int>>t(r-l+1);
-        int i=l,j=m+1;
-        int k=0;
+        int i=l,j=m+1,k=0;
+        
         while(i<=m&&j<=r){
-            if(arr[i].first>arr[j].first)
-            {
-                cnt[arr[i].second]+=r-j+1;
-                t[k++]=arr[i++];
-                
+            if(a[i].first>a[j].first){
+                cnt[a[i].second]+=(r-j+1);
+                t[k++]=a[i++];
             }
             else
-                t[k++]=arr[j++];
-            
+                t[k++]=a[j++];
         }
         
         while(i<=m)
-            t[k++]=arr[i++];
+            t[k++]=a[i++];
         while(j<=r)
-            t[k++]=arr[j++];
+            t[k++]=a[j++];
         
-        for(i=l;i<=r;i++)
-            arr[i]=t[i-l];
+        for(int i=l;i<=r;i++)
+            a[i]=t[i-l];
         
     }
     
-    void mergeSort(vector<pair<int,int>>&arr,int l,int r,vector<int>&cnt){
-        
+    void mergeSort(vector<pair<int,int>>&a,vector<int>&cnt,int l,int r){
         if(l<r){
             int m=(l+r)/2;
-            mergeSort(arr,l,m,cnt);
-            mergeSort(arr,m+1,r,cnt);
-            merge(arr,l,m,r,cnt);
+            mergeSort(a,cnt,l,m);
+            mergeSort(a,cnt,m+1,r);
+            merge(a,cnt,l,m,r);
         }
-        
     }
     
     vector<int> countSmaller(vector<int>& nums) {
         
         int n=nums.size();
         vector<int>cnt(n,0);
-        vector<pair<int,int>>arr;
         
+        vector<pair<int,int>>a;
         for(int i=0;i<n;i++){
-            arr.push_back({nums[i],i});
+            a.push_back({nums[i],i});
         }
         
-        mergeSort(arr,0,n-1,cnt);
+        mergeSort(a,cnt,0,n-1);
         return cnt;
-        
     }
 };
