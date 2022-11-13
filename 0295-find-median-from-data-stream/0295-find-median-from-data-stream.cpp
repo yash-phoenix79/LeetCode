6efc2b818db63e98912daf5c16ce6ee08d29bc7a
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-11-13 19:56:42 +0530

    Time: 847 ms (15.11%), Space: 117.1 MB (65.01%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,29 +1,40 @@
 class MedianFinder {
-private:
-    priority_queue<int> firstQ; // max_heap for the first half
-    priority_queue<int, std::vector<int>, std::greater<int> > secQ; // min_heap for the second half
 public:
-    // Adds a number into the data structure.
-    void addNum(int num) {
-        if(firstQ.empty() || (firstQ.top()>num)) firstQ.push(num); // if it belongs to the smaller half
-        else secQ.push(num); 
+    
+    priority_queue<int>pq1;
+    priority_queue<int,vector<int>,greater<int>>pq2;
+    
+    MedianFinder() {
         
-        // rebalance the two halfs to make sure the length difference is no larger than 1
-        if(firstQ.size() > (secQ.size()+1))
-        {
-            secQ.push(firstQ.top());
-            firstQ.pop();
-        }
-        else if(firstQ.size()+1<secQ.size())
-        {
-            firstQ.push(secQ.top());
-            secQ.pop();
-        }
     }
-
-    // Returns the median of current data stream
+    
+    void addNum(int num) {
+        
+        if(pq1.empty()||num<pq1.top())pq1.push(num);
+        else
+            pq2.push(num);
+        if(pq1.size()>pq2.size()+1){
+            pq2.push(pq1.top());
+            pq1.pop();
+        }
+       if(pq2.size()>pq1.size()+1){
+            pq1.push(pq2.top());
+            pq2.pop();
+        }
+        
+    }
+    
     double findMedian() {
-        if(firstQ.size() == secQ.size()) return firstQ.empty()?0:( (firstQ.top()+secQ.top())/2.0);
-        else return (firstQ.size() > secQ.size())? firstQ.top():secQ.top(); 
+        
+        if(pq1.size()==pq2.size())return pq1.empty()?0:(double)(pq1.top()+pq2.top())/2.0;
+        return pq1.size()>pq2.size()?pq1.top():pq2.top();
+        
     }
 };
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
