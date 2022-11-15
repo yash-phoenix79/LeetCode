a4ff775ed344e8fc7495842edc73a21e1860e45d
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-11-15 18:56:21 +0530

    Time: 179 ms (91.59%), Space: 20 MB (68.44%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,47 +1,48 @@
 class Solution {
 public:
     
+    #define pii pair<int,pair<int,int>>
+    
     int d[5]={1,0,-1,0,1};
     
     int minimumEffortPath(vector<vector<int>>& h) {
-       
-        int n=h.size(),m=h[0].size();
         
-        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>>pq;
+        int n=h.size();
+        int m=h[0].size();
+        
+        priority_queue<pii,vector<pii>,greater<pii>>q;
+        q.push({0,{0,0}});
         
         vector<vector<int>>vis(n,vector<int>(m,INT_MAX));
-        
         vis[0][0]=0;
         
-        pq.push({0,0,0});
-        
-        while(!pq.empty()){
+        while(!q.empty()){
+            auto cur=q.top();
+            q.pop();
+            int dis=cur.first;
+            int x=cur.second.first;
+            int y=cur.second.second;
             
-            auto cur=pq.top();
-            pq.pop();
-            
-            int dis=cur[0],x=cur[1],y=cur[2];
-            
-            if(x==n-1&&y==m-1)return dis;
+            if(x==n-1&&y==m-1)
+                return dis;
             
             for(int i=0;i<4;i++){
+                
                 int dx=x+d[i];
                 int dy=y+d[i+1];
                 
                 if(dx<0||dy<0||dx>=n||dy>=m)continue;
-                
-                int nd=max(dis,abs(h[dx][dy]-h[x][y]));
-                
-                if(nd<vis[dx][dy]){
-                    vis[dx][dy]=nd;
-                    pq.push({nd,dx,dy});
+                int val=max(dis,abs(h[dx][dy]-h[x][y]));
+                if(val<vis[dx][dy]){
+                    vis[dx][dy]=val;
+                    q.push({val,{dx,dy}});
                 }
-                
                 
             }
             
         }
-    
+        
         return 0;
+        
     }
 };
