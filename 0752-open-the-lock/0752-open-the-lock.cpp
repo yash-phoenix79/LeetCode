b54437df84b84c08bea8059c9b043391c118f328
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-11-18 12:52:43 +0530

    Time: 337 ms (72.64%), Space: 34.6 MB (75.52%) - LeetHub

--- 0752-open-the-lock/0752-open-the-lock.cpp
+++ 0752-open-the-lock/0752-open-the-lock.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
-    int openLock(vector<string>& dead, string tar) {
-       
-        unordered_set<string>de(begin(dead),end(dead));
+    int openLock(vector<string>& de, string tar) {
+        
+        unordered_set<string>ds(begin(de),end(de));
         unordered_set<string>vis;
         
-         int res=0;
-        
-        if(de.find("0000")!=de.end())
+        if(ds.find("0000")!=ds.end())
             return -1;
         
         queue<string>q;
         q.push("0000");
+        
         vis.insert("0000");
+        
+        int res=0;
         
         while(!q.empty()){
             
@@ -23,19 +24,21 @@ public:
                 
                 if(t==tar)
                     return res;
+                char u,d;
                 for(int i=0;i<4;i++){
-                    up=t,down=t;
-                    char u=up[i],d=down[i];
                     
-                    up[i]=(u=='9'?'0':u+1);
-                    down[i]=(d=='0'?'9':d-1);
+                    up=down=t;
+                    u=d=t[i];
+                    
+                    up[i]=(u=='9')?'0':u+1;
+                    down[i]=(d=='0')?'9':d-1;
+                    
+                  if(vis.find(up)==vis.end()&&ds.find(up)==ds.end()){
+                      vis.insert(up);
+                      q.push(up);
+                  }  
                    
-                    if(vis.find(up)==vis.end()&&(de.find(up)==de.end())){
-                        vis.insert(up);
-                        q.push(up);
-                    }
-                    
-                    if(vis.find(down)==vis.end()&&(de.find(down)==de.end())){
+                    if(vis.find(down)==vis.end()&&ds.find(down)==ds.end()){
                         vis.insert(down);
                         q.push(down);
                     }
