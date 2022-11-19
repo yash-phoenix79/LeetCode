6db6585f4c0310ad7261f624c4cc94d8e21b2756
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-11-19 19:46:17 +0530

    Time: 774 ms (5.05%), Space: 8.3 MB (45.97%) - LeetHub

--- 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
+++ 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
@@ -4,24 +4,19 @@ public:
     vector<vector<int>>dp;
     
     int find(int l,int r){
-        if(l>=r)return 0;
         
-        if(dp[l][r]!=INT_MAX)
-            return dp[l][r];
+        if(l>=r)return 0;
+        if(dp[l][r]!=INT_MAX)return dp[l][r];
         
         for(int i=l;i<=r;i++)
             dp[l][r]=min(dp[l][r],i+max(find(l,i-1),find(i+1,r)));
-        
         return dp[l][r];
-        
     }
     
     int getMoneyAmount(int n) {
-        
         dp=vector<vector<int>>(n+1,vector<int>(n+1,INT_MAX));
         
         return find(1,n);
-        
         
     }
 };
