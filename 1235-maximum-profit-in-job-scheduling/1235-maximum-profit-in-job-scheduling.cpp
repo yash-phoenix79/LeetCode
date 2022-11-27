e5ecfb7b76c00ed46150c0aba7f7f69c94ee8211
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-11-27 15:32:30 +0530

    Time: 429 ms (30.19%), Space: 55.4 MB (65.72%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -1,18 +1,36 @@
 class Solution {
 public:
-    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
-        int n = startTime.size();
-        vector<vector<int>> jobs;
-        for (int i = 0; i < n; ++i) {
-            jobs.push_back({endTime[i], startTime[i], profit[i]});
-        }
-        sort(jobs.begin(), jobs.end());
-        map<int, int> dp = {{0, 0}};
-        for (auto& job : jobs) {
-            int cur = prev(dp.upper_bound(job[1]))->second + job[2];
-            if (cur > dp.rbegin()->second)
-                dp[job[0]] = cur;
-        }
-        return dp.rbegin()->second;
+    
+    vector<int>dp;
+    
+    int find(int i,vector<vector<int>>&sch,vector<int>& s){
+        int n=s.size();
+        if(i>=n)return 0;
+        
+        if(dp[i]!=-1)
+            return dp[i];
+        
+        int idx=lower_bound(begin(s),end(s),sch[i][1])-begin(s);
+        int tak=sch[i][2]+find(idx,sch,s);
+        int nTak=find(i+1,sch,s);
+        return dp[i]=max(tak,nTak);
+        
+    }
+    
+    int jobScheduling(vector<int>& s, vector<int>& e, vector<int>& p) {
+        
+        int n=s.size();
+        
+        vector<vector<int>>sch(n);
+        dp=vector<int>(n,-1);
+        
+        for(int i=0;i<n;i++)
+            sch[i]={s[i],e[i],p[i]};
+        
+        sort(begin(sch),end(sch));
+        sort(begin(s),end(s));
+        
+        return find(0,sch,s);
+        
     }
 };
