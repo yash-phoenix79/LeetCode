8f2c0d1e9020b581d43473e40c48f151a78626b0
yash-phoenix79 <71338071+yash-phoenix79@users.noreply.github.com>
2022-12-23 20:46:32 +0530

    Time: 309 ms (83.92%), Space: 94.2 MB (38.00%) - LeetHub

--- 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.cpp
+++ 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.cpp
@@ -1,36 +1,48 @@
 class Solution {
 public:
-    vector<unordered_set<int>> tree;
-    vector<int> res, count;
-
-    vector<int> sumOfDistancesInTree(int N, vector<vector<int>>& edges) {
-        tree.resize(N);
-        res.assign(N, 0);
-        count.assign(N, 1);
-        for (auto e : edges) {
-            tree[e[0]].insert(e[1]);
-            tree[e[1]].insert(e[0]);
+    
+    vector<vector<int>>adj;
+    vector<int>cnt,res;
+    
+    
+    void dfs(int v,int p=-1){
+        
+        for(auto x:adj[v]){
+            if(x==p)continue;
+            dfs(x,v);
+            cnt[v]+=cnt[x];
+            res[v]+=res[x]+cnt[x];
         }
-        dfs(0, -1);
-        dfs2(0, -1);
+        cnt[v]++;
+    }
+    
+    void dfs2(int v,int n,int p=-1){
+        
+        for(auto x:adj[v]){
+            if(x==p)continue;
+            res[x]=res[v]-cnt[x]+n-cnt[x];
+            dfs2(x,n,v);
+        }
+        
+    }
+    
+    vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& e) {
+        
+        adj.resize(n);
+        
+        for(auto x:e){
+            
+            adj[x[0]].push_back(x[1]);
+            adj[x[1]].push_back(x[0]);
+            
+        }
+        
+        cnt.resize(n);
+        res.resize(n);
+        
+        dfs(0);
+        dfs2(0,n);
+        
         return res;
-
-    }
-
-    void dfs(int root, int pre) {
-        for (auto i : tree[root]) {
-            if (i == pre) continue;
-            dfs(i, root);
-            count[root] += count[i];
-            res[root] += res[i] + count[i];
-        }
-    }
-
-    void dfs2(int root, int pre) {
-        for (auto i : tree[root]) {
-            if (i == pre) continue;
-            res[i] = res[root] - count[i] + count.size() - count[i];
-            dfs2(i, root);
-        }
     }
 };
